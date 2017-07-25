0ec929be6d4b4b28ccf03f58b6c50c4cea0effaf
ToF <cthibaut@octo.com>
2017-07-25 11:42:08 +0200

    does not pass SPOJ because some dash lines are incorrect

---
+++ Arith/SPOJArith.hs
@@ -0,0 +1,47 @@
+import Data.Char
+
+addition = sumOperation (+) '+' 
+subtraction = sumOperation (-) '-'
+
+format x s = reverse (take x (reverse s ++ repeat ' ')) 
+
+line x = take x (repeat '-')
+
+sumOperation op sym x y = map (format w) [a,b,line w,c]
+    where
+    a = show x
+    b = sym:show y
+    c = show (x `op` y)
+    w = maximum $ map length [a,b,c]
+
+multiplication x y = map (format w) [a,b]++ms++[format w c]
+    where
+    a = show x
+    b = '*':show y
+    c = show (x * y)
+    w1 = length b
+    w  = maximum (map length [a,b,c])
+    ms = case y > 9 of
+        True -> format w (line w1) : zipWith pad [0..] (mults x y) ++ [line w]
+        False -> [] ++ [line w]
+    pad p n = format (w-p) (show n)
+    mults :: Integer -> Integer -> [Integer] 
+    mults x y = map (*x) (reverse (map (fromIntegral.digitToInt) (show y)))  
+
+operation s = case sym of
+    '+' -> sumOperation (+) '+' x y
+    '-' -> sumOperation (-) '-' x y
+    '*' -> multiplication x y
+    where 
+    sx  = takeWhile (isDigit) s
+    l   = drop (length sx) s
+    sym = head l
+    sy  = tail l
+    x   = read sx
+    y   = read sy
+
+solve (n:ss) = concatMap ((++[""]).operation) ss 
+
+process = unlines . solve . lines
+
+main = interact process
---
+++ Arith/Specs.hs
@@ -0,0 +1,64 @@
+import Test.Hspec
+import Arith
+
+main = hspec $ do
+    describe "addition" $ do
+        it "shows addition of 2 integer" $ do
+            addition 42 4807 `shouldBe` ["   42"
+                                        ,"+4807"
+                                        ,"-----"
+                                        ," 4849"]
+            addition 4807 42 `shouldBe` ["4807"
+                                        ," +42"
+                                        ,"----"
+                                        ,"4849"]
+    describe "subtraction" $ do
+        it "shows subtraction of 2 integers" $ do
+            subtraction 4807 42 `shouldBe` ["4807"
+                                           ," -42"
+                                           ,"----"
+                                           ,"4765"]
+    describe "multiplication" $ do
+        it "shows multiplication of 2 integers" $ do
+            multiplication 4807 420 `shouldBe` ["   4807"
+                                               ,"   *420"
+                                               ,"   ----"
+                                               ,"      0"
+                                               ,"  9614"
+                                               ,"19228"
+                                               ,"-------"
+                                               ,"2018940"]
+        it "does not show intermediate computations when only one" $ do
+            multiplication 4807 2 `shouldBe` ["4807"
+                                             ,"  *2"
+                                             ,"----"
+                                             ,"9614"]
+
+    describe "operation" $ do
+        it "shows operation given an input" $ do
+            operation "4807-42" `shouldBe` subtraction 4807 42
+
+    describe "solve" $ do
+        it "solve several problems" $ do
+            solve ["2","4807+42","4807-42"] `shouldBe` ["4807"
+                                                       ," +42"
+                                                       ,"----"
+                                                       ,"4849"
+                                                       ,""
+                                                       ,"4807"
+                                                       ," -42"
+                                                       ,"----"
+                                                       ,"4765"
+                                                       ,""]
+
+    describe "process" $ do
+        it "process input and show output" $ do
+            process "2\n4807+42\n4807-42\n" `shouldBe` 
+                "4807\n +42\n----\n4849\n\n4807\n -42\n----\n4765\n\n"
+            
+
+
+                                              
+                                                
+
+
