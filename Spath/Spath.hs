3d17212f67383fd3227ac1e1a53637d3c7177229
ToF- <cthibaut@octo.com>
2017-08-01 10:12:56 -0400

    edge weighted graph

--- Spath/Spath.hs
+++ Spath/Spath.hs
@@ -5,29 +5,13 @@ solve _ = [3,2,3,2]
 
 process = unlines . Prelude.map show . solve . lines
 
-type RoadMap = Map Number [Road]
-type Road = (Number,Distance)
-type Name = String
-type Number = Int
-type Distance = Int
 
-
-emptyRoads :: RoadMap
-emptyRoads = empty
-
-addRoad :: Number -> Number -> Distance -> RoadMap -> RoadMap
-addRoad f t d rm = insertWith (++) f [(t,d)] rm
-
-lookupRoads :: Number -> RoadMap -> [Road]
-lookupRoads n rm = case M.lookup n rm of
-    Nothing -> []
-    Just rs -> rs
-
+type EdgeWeightedGraph a b = Map a [(a,b)] 
 data Heap a = Empty | Heap a [Heap a]
     deriving (Eq,Show)
 
-emptyHeap :: Heap a
-emptyHeap = Empty
+empty :: Heap a
+empty = Empty
 
 isEmpty :: Heap a -> Bool
 isEmpty Empty = True
@@ -44,9 +28,11 @@ merge h1@(Heap x sh1) h2@(Heap y sh2)
     | x < y     = Heap x (h2:sh1)
     | otherwise = Heap y (h1:sh2)
 
-fromList :: Ord(a) => [a] -> Heap a
-fromList [] = Empty
-fromList (x:xs) = merge (Heap x []) (Spath.fromList xs)
+insert :: Ord(a) => a -> Heap a -> Heap a
+insert a h = merge (Heap a []) h
+
+heapFromList :: Ord(a) => [a] -> Heap a
+heapFromList = Prelude.foldr Spath.insert Spath.empty
 
 deleteMin :: Ord(a) => Heap a -> Heap a
 deleteMin (Heap x hs) = mergePairs hs
@@ -54,3 +40,9 @@ deleteMin (Heap x hs) = mergePairs hs
     mergePairs [] = Empty
     mergePairs [h] = h
     mergePairs (h1:h2:hs) = merge (merge h1 h2) (mergePairs hs)
+
+graphFromList :: Ord(a) => [(a,[(a,b)])] -> EdgeWeightedGraph a b
+graphFromList = M.fromList  
+
+neighbors :: Ord(a) => a -> EdgeWeightedGraph a b -> Maybe [(a,b)]
+neighbors = M.lookup
--- Spath/Specs.hs
+++ Spath/Specs.hs
@@ -1,41 +1,42 @@
 import Test.Hspec
-import Spath
+import Spath as S
+import Data.Map as M
 
 main = hspec $ do
 
-    describe "roads" $ do
-        it "collects the roads from a city to other cities" $ do
-            let rs = addRoad 1 2 42
-                      $ addRoad 1 3 17
-                      $ addRoad 2 3 18
-                      $ emptyRoads
-            lookupRoads 1 rs `shouldBe` [(2,42),(3,17)]
-            lookupRoads 2 rs `shouldBe` [(3,18)]
-            lookupRoads 3 rs `shouldBe` []
-
     describe "heap" $ do
         it "can be empty" $ do
-            isEmpty emptyHeap  `shouldBe` True
+            isEmpty S.empty  `shouldBe` True
             isEmpty (Heap 42 []) `shouldBe` False
 
         it "have a min value" $ do
-            findMin (Heap 42 [Heap 4807 []]) `shouldBe` Just 42
-            findMin (Empty::Heap Int) `shouldBe` Nothing
+            S.findMin (Heap 42 [Heap 4807 []]) `shouldBe` Just 42
+            S.findMin (Empty::Heap Int) `shouldBe` Nothing
 
         it "can be merged, and still be ordered" $ do
             let h = Heap 42 []
                 i = Heap 4807 []
-            findMin (merge h i) `shouldBe` Just 42
+            S.findMin (merge h i) `shouldBe` Just 42
 
+        it "can be insert a value" $ do
+            let h = S.insert 42 S.empty
+            S.findMin h `shouldBe` Just 42
 
-        it "can be constructed from a list" $ do
-            let h = fromList [42,17,4807]
-            findMin h `shouldBe` Just 17
+        it "can be built from a list" $ do
+            let h = S.heapFromList [42,17,4807]
+            S.findMin h `shouldBe` Just 17
 
         it "can have is min deleted and still be ordered" $ do
-            let h = fromList [42,17,4807,3]
-            findMin h `shouldBe` Just 3
-            let h'=deleteMin h
-            findMin h' `shouldBe` Just 17
-    
+            let h = S.heapFromList [42,17,4807,3]
+            S.findMin h `shouldBe` Just 3
+            let h'=S.deleteMin h
+            S.findMin h' `shouldBe` Just 17
+
+    describe "edge weighted graph" $ do
+        it "can be build from a list of valued edges" $ do
+            let g = graphFromList [(1, [(2, 100.0),(3, 150.0)])
+                                  ,(2, [(4, 400.0)])
+                                  ,(3, [(4,  50.0)])]
+            neighbors 1 g `shouldBe` Just [(2, 100.0)
+                                          ,(3, 150.0)]
     
