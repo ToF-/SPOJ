6ca84b9a9a5237bfa941afe2d0287166eed41995
Christophe Thibaut <cthibaut@octo.com>
2019-08-25 18:52:25 +0200

    tested minView

--- Spath/Specs.hs
+++ Spath/Specs.hs
@@ -13,9 +13,16 @@ instance Arbitrary Prio where
     arbitrary = fmap PR (choose (0,1000))
 
 main = hspec $ do
-    let bindings = fmap (take 100) arbitrary :: Gen [(Key,Prio)]
+    let bindings = fmap (take 3) arbitrary :: Gen [(Key,Prio)]
     describe "Priority Search Queue" $ do
         it "finds a key that is in the Queue" $ do
             forAll bindings $ \bs ->
                let q = fromList bs
                in all (\k -> lookupPSQ k q /= Nothing) $ map key bs 
+
+        it "yields the maximal priority" $ do
+            forAll bindings $ \bs ->
+                let q = fromList bs
+                    l = toOrdList q
+                    Min b _ = minView q
+                in q == Void || prio b == minimum (map prio l)
