020c920faa0dd7b333efe9ef6f45416d55ed5baf
ToF- <cthibaut@octo.com>
2019-09-03 11:12:04 +0200

    amortized

--- FCTRL2/C/factorial.c
+++ FCTRL2/C/factorial.c
@@ -23,24 +23,22 @@ int get_int() {
 
 int main() {
     int max_cases = get_int();
-    int MaxN = 1;
+    int MaxN = 0;
     Results[MaxN].value[0]=1;
     Results[MaxN].size=1;
     for(int c=0; c<max_cases; c++) {
-        int n = get_int();
+        int n = get_int()-1;
         if (n > MaxN) {
             for(int m = MaxN; m < n; m++) { 
-                printf("copy %d into %d\n", m, m+1);
                 for(int i = 0; i < Results[m].size; i++)
                     Results[m+1].value[i] = Results[m].value[i];
                 Results[m+1].size = Results[m].size;
-                printf("multiply %d by %d\n", m, m+1);
                 int carry = 0;
                 for(int i = 0; i < Results[m+1].size; i++) {
-                    int prod = Results[m].value[m+1] * (m+1) + carry;
-                        Results[m+1].value[i] = prod % 10;
-                        carry = prod / 10;
-                    }
+                    int prod = Results[m+1].value[i] * (m+2) + carry;
+                    Results[m+1].value[i] = prod % 10;
+                    carry = prod / 10;
+                }
                 while(carry) {
                     Results[m+1].value[Results[m+1].size] = carry % 10;
                     carry = carry / 10;
@@ -49,8 +47,8 @@ int main() {
                 MaxN=m+1;
             } 
         }
-        for(int i = Results[MaxN].size-1; i>=0;i--) 
-            printf("%c", Results[MaxN].value[i]+'0');
+        for(int i = Results[n].size-1; i>=0;i--) 
+            printf("%c", Results[n].value[i]+'0');
         printf("\n");
     }
     return 0;
