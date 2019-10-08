430664ec48e51fe7367a72feec41ce5963f38e5b
ToF- <cthibaut@octo.com>
2019-10-08 14:37:52 +0200

    adjacent distances on a min heap with several cells

--- BITMAP/Haskell/Bitmap.hs
+++ BITMAP/Haskell/Bitmap.hs
@@ -1,5 +1,6 @@
 module Bitmap
 where
+import Data.List (sort)
 
 type Bit = Char
 type Coord = (Int,Int) 
@@ -23,11 +24,13 @@ fromList = MinHeap
 
 adjacent :: MinHeap CoordD -> MinHeap CoordD
 adjacent mh = let (CoordD (x,y) d,mh') = extractMin mh
-    in foldr (addAdjacent d) (fromList []) [(x,y-1),(x,y+1),(x-1,y),(x+1,y)]
+    in foldr (addAdjacent d) mh' [(x,y-1),(x,y+1),(x-1,y),(x+1,y)]
     where
     addAdjacent :: Distance -> Coord -> MinHeap CoordD -> MinHeap CoordD
     addAdjacent d cd (MinHeap as) = MinHeap (as++[cd-:(d+1)])
 
 extractMin :: MinHeap CoordD -> (CoordD, MinHeap CoordD)
-extractMin (MinHeap as) = (head as, MinHeap (tail as))
+extractMin (MinHeap as) = (head as', MinHeap (tail as'))
+    where
+    as' = sort as
 
--- BITMAP/Haskell/Specs.hs
+++ BITMAP/Haskell/Specs.hs
@@ -10,5 +10,10 @@ main = hspec $ do
                     result  = sort $ toList (adjacent initial)
                     expected = sort [(2,1)-:4, (2,3)-:4, (1,2)-:4, (3,2)-:4] 
                 result `shouldBe` expected
+            it "on the smallest distance cell" $ do
+                let initial = fromList [(7,2)-:6, (2,2)-:3]
+                    result  = sort $ toList (adjacent initial)
+                    expected = sort [(2,1)-:4, (2,3)-:4, (1,2)-:4, (3,2)-:4, (7,2)-:6] 
+                result `shouldBe` expected
 
 
