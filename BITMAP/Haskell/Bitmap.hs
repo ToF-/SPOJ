281634fb1f987d87b975388a78528241f69af0fd
ToF- <cthibaut@octo.com>
2019-10-08 14:34:13 +0200

    adjacent, using a minheap

--- BITMAP/Haskell/Bitmap.hs
+++ BITMAP/Haskell/Bitmap.hs
@@ -3,15 +3,31 @@ where
 
 type Bit = Char
 type Coord = (Int,Int) 
+type Distance = Int
 
 data CoordD = CoordD Coord Int
+    deriving (Eq, Ord, Show)
 data MinHeap a = MinHeap [a]
+    deriving (Eq, Show)
 
 (-:) :: Coord -> Int -> CoordD
 (-:) = CoordD
 
+infixl 6 -:
+
 toList :: MinHeap CoordD -> [CoordD]
 toList (MinHeap as) = as
 
+fromList :: [CoordD] -> MinHeap CoordD
+fromList = MinHeap
 
+adjacent :: MinHeap CoordD -> MinHeap CoordD
+adjacent mh = let (CoordD (x,y) d,mh') = extractMin mh
+    in foldr (addAdjacent d) (fromList []) [(x,y-1),(x,y+1),(x-1,y),(x+1,y)]
+    where
+    addAdjacent :: Distance -> Coord -> MinHeap CoordD -> MinHeap CoordD
+    addAdjacent d cd (MinHeap as) = MinHeap (as++[cd-:(d+1)])
+
+extractMin :: MinHeap CoordD -> (CoordD, MinHeap CoordD)
+extractMin (MinHeap as) = (head as, MinHeap (tail as))
 
--- BITMAP/Haskell/Specs.hs
+++ BITMAP/Haskell/Specs.hs
@@ -1,10 +1,14 @@
 import Test.Hspec
 import Bitmap
+import Data.List (sort)
 
 main = hspec $ do
-    describe "distances compute distances to pixels ON" $ do
-
-        it "on a single pixel map" $ do
-            toList (distances ["1"]) `shouldBe` [(0,0) -: 0]
+    describe "adjacent" $ do
+        describe "find distances of cells adjacent to a cell" $ do
+            it "on a single cell" $ do
+                let initial = fromList [(2,2)-:3]
+                    result  = sort $ toList (adjacent initial)
+                    expected = sort [(2,1)-:4, (2,3)-:4, (1,2)-:4, (3,2)-:4] 
+                result `shouldBe` expected
 
 
