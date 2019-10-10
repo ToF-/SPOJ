ccfaf4842702b5d0eee72af67a9811ca73a901af
Christophe Thibaut <cthibaut@octo.com>
2019-10-10 13:53:58 +0200

    a distance map can be filled with distances

--- BITMAP/Haskell/Bitmap.hs
+++ BITMAP/Haskell/Bitmap.hs
@@ -1,13 +1,19 @@
 module Bitmap
 where
+import Data.Map as M
 
 type Size = Coord
 type Coord = (Int,Int)
 type Distance = Int
-data DistanceMap = Nil
+data DistanceMap = DM Size (M.Map Coord Distance)
 
 distanceMap :: Size -> DistanceMap
-distanceMap _ = Nil
+distanceMap hw = DM hw M.empty
 
 at :: DistanceMap -> Coord -> Maybe Distance
-dm `at` ij = Nothing
+(DM hw m) `at` ij | ij `within` hw = M.lookup ij m
+    where
+    (i,j) `within` (h,w) = i < h && j < w && i >= 0 && j >= 0
+
+set :: Coord -> Distance -> DistanceMap -> DistanceMap
+set ij d (DM hw m) = DM hw (insert ij d m)
--- BITMAP/Haskell/Specs.hs
+++ BITMAP/Haskell/Specs.hs
@@ -7,3 +7,7 @@ main = hspec $ do
         it "is initially filled with Nothing" $ do
             let dm = distanceMap (1,1)
             dm `at` (0,0) `shouldBe` Nothing
+
+        it "can be filled with distances" $ do
+            let dm = set (0,0) 4 $ distanceMap (2,2)
+            dm `at` (0,0) `shouldBe` Just 4
