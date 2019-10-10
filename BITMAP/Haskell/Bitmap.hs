70cbc3a8b4123458636c141a9234c30027e62eb1
Christophe Thibaut <cthibaut@octo.com>
2019-10-10 13:45:47 +0200

    a distance map is initially filled with nothing

--- BITMAP/Haskell/Bitmap.hs
+++ BITMAP/Haskell/Bitmap.hs
@@ -1,2 +1,13 @@
 module Bitmap
 where
+
+type Size = Coord
+type Coord = (Int,Int)
+type Distance = Int
+data DistanceMap = Nil
+
+distanceMap :: Size -> DistanceMap
+distanceMap _ = Nil
+
+at :: DistanceMap -> Coord -> Maybe Distance
+dm `at` ij = Nothing
--- BITMAP/Haskell/Specs.hs
+++ BITMAP/Haskell/Specs.hs
@@ -3,3 +3,7 @@ import Bitmap
 import Data.List (sort)
 
 main = hspec $ do
+    describe "a distance map" $ do
+        it "is initially filled with Nothing" $ do
+            let dm = distanceMap (1,1)
+            dm `at` (0,0) `shouldBe` Nothing
