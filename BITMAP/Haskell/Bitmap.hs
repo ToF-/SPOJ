4e7b6376748551143f281e5208c9ad0a52f4f8f0
Christophe Thibaut <cthibaut@octo.com>
2019-10-10 21:32:24 +0200

    distanceMap can be added a distance

--- BITMAP/Haskell/Bitmap.hs
+++ BITMAP/Haskell/Bitmap.hs
@@ -1,14 +1,19 @@
 module Bitmap where
 
+import Data.Map as M (Map, empty, insert, lookup)
+
 type Size     = Coord
 type Coord    = (Int,Int)
 type Distance = Int
-data DistanceMap = Nil
+data DistanceMap = DM Size (Map Coord Distance)
     deriving (Show)
 
 
 distanceMap :: Size -> DistanceMap
-distanceMap hw = Nil
+distanceMap hw = DM hw M.empty
 
 at :: DistanceMap -> Coord -> Maybe Distance
-Nil `at` ij = Nothing
+(DM _ m) `at` ij = M.lookup ij m 
+
+set :: Coord -> Distance -> DistanceMap -> DistanceMap
+set ij d (DM hw m) = DM hw (M.insert ij d m)
--- BITMAP/Haskell/Specs.hs
+++ BITMAP/Haskell/Specs.hs
@@ -7,4 +7,6 @@ main = hspec $ do
         it "is initially filled with Nothing" $ do
             let dm = distanceMap (1,1)
             dm `at` (0,0) `shouldBe` Nothing
-
+        it "can be added a distance" $ do
+            let dm = set (0,0) 3 $ distanceMap (1,1)
+            dm `at` (0,0) `shouldBe` Just 3
