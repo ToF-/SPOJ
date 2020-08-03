6d378cc47ee35d1c1e030883decb758e94501368
Christophe Thibaut <cthibaut@octo.com>
2020-08-03 10:15:12 +0200

    refactoring for space, usins 2 digits per byte in big int

--- FCTRL2/Forth/factorial.fs
+++ FCTRL2/Forth/factorial.fs
@@ -16,24 +16,47 @@ CREATE BIGINT MAXBYTES ALLOT
         OVER C@                 \ addr,c,d
         R@ SWAP                 \ addr,c,f,d
         * +                     \ addr,f*d+c
-        10 /MOD SWAP            \ addr,c,d
+        100 /MOD SWAP           \ addr,c,d
         ROT DUP >R C! R>        \ c,addr
         1- SWAP                 \ addr,c
         OVER BIGINT             \ addr,c,addr,start
     < UNTIL 
     R> DROP DROP DROP ;
 
+: FIND-DIGIT? ( limit,addr -- addr,f )
+    1-
+    BEGIN 
+        1+ OVER OVER = 
+        OVER C@ OR
+    UNTIL
+    DUP ROT < ;
+
+
+: .1DIGIT ( b -- )
+    [CHAR] 0 + EMIT ;
+
+: .2DIGITS ( b -- )
+    10 /MOD .1DIGIT .1DIGIT ;
+    
+: .FIRST-DIGIT ( b -- )
+    DUP 10 > IF .2DIGITS ELSE .1DIGIT THEN ;
+
+: .DIGITS ( limit,addr -- )
+    DO I C@ .2DIGITS LOOP ;
 
 : PRINT-BIGINT 
-    BIGINT MAXBYTES + BIGINT 1-
-    BEGIN 
-        1+
-        2DUP = 
-        OVER C@ 
-    OR UNTIL
-    DO
-        I C@ [CHAR] 0 + EMIT
-    LOOP ;
+    BIGINT MAXBYTES + 
+    DUP BIGINT
+    FIND-DIGIT? IF
+        DUP C@ .FIRST-DIGIT
+        1+ OVER OVER > IF
+            .DIGITS
+        ELSE
+            DROP DROP
+        THEN
+    ELSE
+        DROP
+    THEN ;
     
 : FACTORIAL ( n -- ) 
     INITIALIZE 
@@ -53,3 +76,4 @@ CREATE BIGINT MAXBYTES ALLOT
     LOOP ;
 
 MAIN BYE
+
