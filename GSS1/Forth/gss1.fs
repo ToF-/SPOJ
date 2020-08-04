20d4db3874745f3c21323c80f4d09bcba02103e9
Christophe Thibaut <cthibaut@octo.com>
2020-08-04 23:45:09 +0200

    merge node

--- GSS1/Forth/gss1.fs
+++ GSS1/Forth/gss1.fs
@@ -1,6 +1,10 @@
 50000 CONSTANT MAX-NUMBER
 4 CELLS CONSTANT NODE-SIZE 
 
+CREATE LEFT-NODE  NODE-SIZE ALLOT
+CREATE RIGHT-NODE NODE-SIZE ALLOT
+CREATE MERGE-NODE NODE-SIZE ALLOT
+
 : NODES ( n -- s )
     4 CELLS * ;
 
@@ -28,9 +32,59 @@ CREATE SEGMENT-TREE MAX-NUMBER TREE-SIZE ALLOT
     DUP CELL+ CELL+ 
     >R 2! R> 2! ;
 
-: MAX-SEG-SUM ( node -- n )
+: MAX-SEG-SUM ( max,sum,pre,suf -- max )
     2DROP DROP ;
 
+: SEG-SUM ( max,sum,pre,suf -- sum )
+    2DROP SWAP DROP ;
+
+: MAX-PREFIX-SUM ( max,sum,pre,suf -- pre )
+    2SWAP 2DROP DROP ;
+
+: MAX-SUFFIX-SUM ( max,sum,pre,suf -- suf )
+    SWAP DROP -ROT 2DROP ;
+
+: >MAX-SEG-SUM ( addr -- addr )
+    CELL+ CELL+ CELL+ ;
+
+: >SEG-SUM     ( addr -- addr )
+    CELL+ CELL+ ;
+
+: >MAX-PRE     ( addr -- addr )
+    CELL+ ;
+
+: >MAX-SUF     ( addr -- addr )
+    ;
+
+: MERGE-MAX-SEG-SUM ( -- max )
+    LEFT-NODE  >MAX-SUF @ 
+    RIGHT-NODE >MAX-PRE @ +
+    RIGHT-NODE >MAX-SEG-SUM @ MAX
+    LEFT-NODE  >MAX-SEG-SUM @ MAX ;
+
+: MERGE-SEG-SUM ( -- sum )
+    LEFT-NODE  >SEG-SUM @
+    RIGHT-NODE >SEG-SUM @ + ;
+
+: MERGE-MAX-PREFIX-SUM ( -- max )
+    LEFT-NODE  >MAX-PRE @
+    LEFT-NODE  >SEG-SUM @
+    RIGHT-NODE >MAX-PRE @ + MAX ;
+
+: MERGE-MAX-SUFFIX-SUM ( -- max )
+    RIGHT-NODE >MAX-SUF @
+    RIGHT-NODE >SEG-SUM @ 
+    LEFT-NODE  >MAX-SUF @ + MAX ;
+
+: MERGE ( node,node -- node )
+    RIGHT-NODE NODE!
+    LEFT-NODE  NODE!
+    MERGE-MAX-SEG-SUM 
+    MERGE-SEG-SUM
+    MERGE-MAX-PREFIX-SUM
+    MERGE-MAX-SUFFIX-SUM ;
+
+    
 
 
 
--- GSS1/Forth/testgss1.fs
+++ GSS1/Forth/testgss1.fs
@@ -1,3 +1,4 @@
+PAGE
 REQUIRE ffl/tst.fs
 REQUIRE gss1.fs
 
@@ -25,4 +26,30 @@ T{ ." MAX-SEG-SUM gives the maximum segment sum member of a node " CR
     42 17 4807 -34 SEGMENT-TREE NODE! 
     SEGMENT-TREE NODE@ MAX-SEG-SUM 42 ?S
 }T
+
+T{ ." OFFSET words give access to node members " CR
+    1 2 3 4 LEFT-NODE NODE!
+    LEFT-NODE >MAX-SEG-SUM @ 1 ?S
+    LEFT-NODE >SEG-SUM     @ 2 ?S
+    LEFT-NODE >MAX-PRE     @ 3 ?S
+    LEFT-NODE >MAX-SUF     @ 4 ?S
+}T
+
+T{ ." MERGE max segment sum = max of left max seg sum, right max seg sum, left max suffix sum + right max prefix sum " CR
+    4807 0 42 17   1024 23 0 10 MERGE    MAX-SEG-SUM 4807 ?S
+    4807 0 42 17   5042 23 0 10 MERGE    MAX-SEG-SUM 5042 ?S
+    7 0 -42 17     2 23 42 -17  MERGE    MAX-SEG-SUM 59 ?S
+}T
+
+T{ ." MERGE segment sum = left segment sum + right segment sum " CR
+    0 4807 42 17   23 1024 0 10 MERGE    SEG-SUM 5831 ?S
+}T
+
+T{ ." MERGE segment prefix sum = max of left max prefix sum, left segment sum + right max prefix sum " CR
+    0 4807 42 17   23 1024 20 10 MERGE   MAX-PREFIX-SUM 4827 ?S
+    0 10000 42 17   23 1024 0 10 MERGE   MAX-PREFIX-SUM 10000 ?S
+}T
+T{ ." MERGE segment suffix sum = max of right suffix sum, right segment sum + left max suffix sum " CR
+    0 4807 42 17   23 1024 20 10 MERGE   MAX-SUFFIX-SUM 1041 ?S
+    0 4807 42 17   23 1024 20 6000 MERGE   MAX-SUFFIX-SUM 6000 ?S
 BYE
