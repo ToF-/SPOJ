ce587ae9b2f599449cde8374e537067a1ea4128f
Christophe Thibaut <cthibaut@octo.com>
2020-08-13 14:59:27 +0200

    get-equation finds the mystery sum and the target sum

--- JEDNAKOS/Forth/jednakos.fs
+++ JEDNAKOS/Forth/jednakos.fs
@@ -5,23 +5,40 @@ VARIABLE MYSTERY-SIZE
 VARIABLE TARGET-SUM
 CREATE   MYSTERY-SUM MYSTERY-MAX ALLOT
 
-: GET-MYSTERY-SUM ( addr,l -- )
-    0 DO
-        DUP I + C@ DUP EQUAL = IF 
-            I MYSTERY-SIZE !
-            DROP
-        LEAVE THEN
-        ZERO - MYSTERY-SUM I + C!
-    LOOP DROP ;
-
+: GET-MYSTERY-SUM ( addr,l -- s )
+    0 0 ROT           \ addr,j,z,l
+    0 DO              \ addr,j,z
+        >R OVER R> SWAP \ addr,j,z,addr
+        I + C@        \ addr,j,z,c
+        DUP EQUAL = IF DROP LEAVE THEN
+        ZERO -        \ addr,j,z,d
+        DUP 0=        \ addr,j,z,d,f
+        SWAP -ROT     \ addr,j,d,z,f
+        IF 
+            1+                  \ addr,j,d,z'
+        ELSE 
+            DROP 0              \ addr,j,d,z'
+        THEN                    \ addr,j,d,z
+        DUP 4 < IF              \ addr,j,d,z
+            -ROT                \ addr,z,j,d
+            OVER MYSTERY-SUM +  \ addr,z,j,d,dest
+            C!                  \ addr,z,j
+            1+                  \ addr,z,j'
+            SWAP                \ addr,j,z
+        ELSE                    \ addr,j,d,z
+            NIP                 \ addr,j,z
+        THEN
+    LOOP                        \ addr,j,z
+    DROP NIP ;
 
 
 : GET-EQUATION ( addr,l -- )
-    OVER OVER GET-MYSTERY-SUM   \ addr,l
-    MYSTERY-SIZE @ 1+ >R        \ addr,l
-    SWAP R@ +                   \ l,addr'
-    SWAP R> -                   \ addr,l'
-    S>NUMBER? IF D>S TARGET-SUM ! ELSE 2DROP THEN
-    ;
+    OVER OVER GET-MYSTERY-SUM   \ addr,l,s
+    DUP MYSTERY-SIZE ! 1+       \ addr,l,s+1
+    DUP >R                      \ addr,l,s+1
+    -                           \ addr,l'
+    SWAP R> +                   \ l',addr'
+    SWAP S>NUMBER?              \ d,f
+    IF D>S TARGET-SUM ! ELSE 2DROP THEN ;
 
 : MAIN 42 . CR ;
--- JEDNAKOS/Forth/testjednakos.fs
+++ JEDNAKOS/Forth/testjednakos.fs
@@ -2,14 +2,19 @@ require ffl/tst.fs
 require jednakos.fs
 
 t{ ." get-equation finds the mystery sum and the target sum" cr
-    s" 0=0" get-equation target-sum   @ 0 ?s mystery-sum c@ 0 ?s mystery-size @ 1 ?s
+    s" 0=0" GET-EQUATION TARGET-SUM   @ 0 ?s MYSTERY-SUM c@ 0 ?s MYSTERY-SIZE @ 1 ?s
 
-    s" 5=5" get-equation target-sum   @ 5 ?s mystery-sum c@ 5 ?s mystery-size @ 1 ?s
+    s" 5=5" GET-EQUATION TARGET-SUM   @ 5 ?s MYSTERY-SUM c@ 5 ?s MYSTERY-SIZE @ 1 ?s
 
-    s" 520=25" get-equation target-sum @ 25 ?s
-    mystery-sum     c@ 5 ?s
-    mystery-sum 1 + c@ 2 ?s
-    mystery-sum 2 + c@ 0 ?s
-    mystery-size   @ 3 ?S
-   }t
+    s" 520=25" GET-EQUATION TARGET-SUM @ 25 ?s
+    MYSTERY-SUM     c@ 5 ?s
+    MYSTERY-SUM 1 + c@ 2 ?s
+    MYSTERY-SUM 2 + c@ 0 ?s
+    MYSTERY-SIZE   @ 3 ?S
+}t
+t{ ." get-equation eliminates zeroes when contiguous and more than 3 " cr
+    s" 42000003=45" GET-EQUATION MYSTERY-SIZE @ 6 ?s
+       
+    s" 0000042000003000000=45" GET-EQUATION MYSTERY-SIZE @ 12 ?s
+}t
 bye
