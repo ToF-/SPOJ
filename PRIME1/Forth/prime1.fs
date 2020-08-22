297fbd765a2450be6b8d738efe42aaf20900019c
Christophe Thibaut <cthibaut@octo.com>
2020-08-22 14:57:18 +0200

    status : broken

--- PRIME1/Forth/prime1.fs
+++ PRIME1/Forth/prime1.fs
@@ -1,8 +1,10 @@
-184        CONSTANT GAMMA
-GAMMA 8 /  CONSTANT SMALL-SET%
-
 : SQUARE ( n -- n^2 )
     DUP * ;
+
+184          CONSTANT GAMMA
+GAMMA 8 /    CONSTANT SMALL-SET%
+GAMMA SQUARE CONSTANT DELTA
+DELTA 8 /    CONSTANT LARGE-SET%
 
 : IS-PRIME? ( n -- flag )
     2 BEGIN
@@ -22,6 +24,9 @@ GAMMA 8 /  CONSTANT SMALL-SET%
 CREATE SMALL-PRIMES GAMMA SMALL-PRIMES,
 HERE SMALL-PRIMES - CONSTANT SMALL-PRIMES% 
 
+DELTA 2* CONSTANT LARGE-PRIMES%
+CREATE LARGE-PRIMES LARGE-PRIMES% ALLOT
+
 : .SMALL-PRIMES 
     SMALL-PRIMES% 0 DO 
         SMALL-PRIMES I + C@ . 
@@ -31,11 +36,21 @@ HERE SMALL-PRIMES - CONSTANT SMALL-PRIMES%
     ASSERT( DUP GAMMA < )
     SMALL-PRIMES + C@ ;
 
+: LARGE-PRIME# ( index -- prime )
+    ASSERT( DUP DELTA < )
+    2* LARGE-PRIMES + W@ ;
+
 DEFER PROC-SMALL-PRIMES 
 
 : EXEC-SMALL-PRIMES ( -- )
     SMALL-PRIMES% 0 DO
         I SMALL-PRIME# PROC-SMALL-PRIMES
+    LOOP ;
+
+: SMALL-PRIMES>LARGE-PRIMES! ( prime -- )
+    SMALL-PRIMES% 0 DO
+            I SMALL-PRIME# 
+            I 2* LARGE-PRIMES + W!
     LOOP ;
 
 : SET-OFFSET ( set,index -- offset )
@@ -55,13 +70,22 @@ DEFER PROC-SMALL-PRIMES
     1 SWAP LSHIFT OR SWAP C! ;
 
 CREATE SMALL-SET SMALL-SET% ALLOT SMALL-SET SMALL-SET% 255 FILL
+CREATE LARGE-SET LARGE-SET% ALLOT LARGE-SET LARGE-SET% 255 FILL
 
 DEFER PROC-SMALL-SET
+DEFER PROC-LARGE-SET
 
 : EXEC-SMALL-SET ( limit -- )
     0 DO 
         SMALL-SET I INCLUDE? IF 
             I PROC-SMALL-SET
+        THEN
+    LOOP ;
+
+: EXEC-LARGE-SET ( limit -- )
+    0 DO 
+        LARGE-SET I INCLUDE? IF 
+            I PROC-LARGE-SET
         THEN
     LOOP ;
 
@@ -84,9 +108,38 @@ DEFER PROC-SMALL-SET
     SIEVE!
     DROP ;
 
-: SIEVES! ( set,limit,start -- )
+: SMALL-SIEVES! ( set,limit,start -- )
+    ROT DUP SMALL-SET% 255 FILL
+    -ROT
     ['] SIEVE-WITH-PRIME! IS PROC-SMALL-PRIMES
     EXEC-SMALL-PRIMES 
     2DROP DROP ;
     
-     
+VARIABLE PRIME#
+VARIABLE START
+
+: SMALL-PRIME>LARGE-PRIME ( offset,index,prime -- offset,index )
+    >R OVER R> +               \ offset,index,prime+offset' 
+    OVER 2*                    \ offset,index,prime',index*2
+    DUP LARGE-PRIMES% < IF 
+        LARGE-PRIMES + W!      \ offset,index
+        1+
+    ELSE
+        2DROP
+    THEN ;
+
+: INIT-LARGE-PRIMES 
+    SMALL-PRIMES>LARGE-PRIMES!
+    SMALL-SET GAMMA GAMMA SMALL-SIEVES!
+    ['] SMALL-PRIME>LARGE-PRIME IS PROC-SMALL-SET 
+    GAMMA SMALL-PRIMES%        \ offset,index
+    GAMMA EXEC-SMALL-SET       \ offset,index
+    GAMMA 2 DO
+        SMALL-SET              \ offset,index,set
+        GAMMA I * GAMMA I 1- * \ offset,index,set,limit,start
+        SMALL-SIEVES!          \ offset,index
+        SWAP GAMMA + SWAP      \ offest+gamma,index
+        GAMMA EXEC-SMALL-SET   \ offset,index
+    LOOP
+    2DROP ;
+
--- PRIME1/Forth/testprime1.fs
+++ PRIME1/Forth/testprime1.fs
@@ -1,4 +1,3 @@
-
 require ffl/tst.fs
 require prime1.fs
 
@@ -30,7 +29,6 @@ t{ ." EXEC-SMALL-PRIMES allows for execution of a word for each small prime " CR
     53 * 59 * 61 * 67 * 71 * 73 * 79 * 83 * 89 * 97 * 101 * 103 * 107 * 109 * 
     113 * 127 * 131 * 137 * 139 * 149 * 151 * 157 * 163 * 167 * 173 * 179 * 181 * ?S
 }t
-
 t{ ." SMALL-SET is a bit set with a small capacity " CR
     SMALL-SET SMALL-SET% 255 FILL
     SMALL-SET 42 INCLUDE? ?true
@@ -48,8 +46,6 @@ t{ ." EXEC-SMALL-SET allows for execution of a word for each bit til a limit tha
     ' * IS PROC-SMALL-SET 1 100 EXEC-SMALL-SET 714 ?S
 }t
 
-: CHECK-IS-PRIME 
-    100 + .  ; \ 100 + . ; \ SMALL-SET + 100 + IS-PRIME? AND ;
 
 t{ ." SIEVE! excludes multiples of a prime in a set " CR
     SMALL-SET SMALL-SET% 255 FILL
@@ -60,15 +56,50 @@ t{ ." SIEVE! excludes multiples of a prime in a set " CR
     SMALL-SET 82 INCLUDE? ?false
 }t
 
-t{ ." SIEVES! excludes multiples of all primes in a set " CR
+t{ ." SMALL-SIEVES! excludes multiples of all primes in a set " CR
     SMALL-SET SMALL-SET% 255 FILL
-    SMALL-SET GAMMA GAMMA SIEVES!
+    SMALL-SET GAMMA GAMMA SMALL-SIEVES!
     SMALL-SET 190 GAMMA - INCLUDE? ?false
     SMALL-SET 191 GAMMA - INCLUDE? ?true
     SMALL-SET 193 GAMMA - INCLUDE? ?true
     SMALL-SET 241 GAMMA - INCLUDE? ?true
     SMALL-SET 293 GAMMA - INCLUDE? ?true
-
 }t
 
+t{ ." LARGE-SET is a bit set with a large capacity " CR
+    LARGE-SET LARGE-SET% 255 FILL
+    LARGE-SET 4807 INCLUDE? ?true
+    LARGE-SET 4807 EXCLUDE!
+    LARGE-SET 4807 INCLUDE? ?false
+}t
+
+t{ ." LARGE-PRIMES is table of 2 bytes word primes that is not initialized " CR
+    0 LARGE-PRIME# 0 ?S
+    1 LARGE-PRIME# 0 ?S
+    2 LARGE-PRIME# 0 ?S
+    LARGE-PRIMES% DELTA 2* ?S
+}t
+
+t{ ." SMALL-PRIMES>LARGE-PRIMES copy the small primes in the large primes table " CR
+    SMALL-PRIMES>LARGE-PRIMES!
+    0 LARGE-PRIME# 2 ?S
+    1 LARGE-PRIME# 3 ?S
+    2 LARGE-PRIME# 5 ?S
+    20 LARGE-PRIME# 73 ?S
+    40 LARGE-PRIME# 179 ?S
+    41 LARGE-PRIME# 181 ?S
+}t
+: CHECK-LARGE-PRIMES DO I LARGE-PRIME# is-prime? 0= IF I . THEN LOOP ;
+
+t{ ." INIT-LARGE-PRIMES finds the large primes and stores them in the large primes table " CR
+    SMALL-PRIMES% 42 ?S
+    SMALL-SET% 23 ?S
+    INIT-LARGE-PRIMES 
+    0  LARGE-PRIME# 2 ?S
+    41 LARGE-PRIME# 181 ?S
+    42 LARGE-PRIME# 191 ?S
+    43 LARGE-PRIME# 193 ?S
+    1000 0 CHECK-LARGE-PRIMES
+
+}t
 BYE
