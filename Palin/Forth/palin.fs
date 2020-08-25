1e3220ba99a6d0b24a48a1f6d3244dcbe6f5cc71
Christophe Thibaut <cthibaut@octo.com>
2020-08-25 22:24:01 +0200

    functions increment extend trim reverse

--- Palin/Forth/palin.fs
+++ Palin/Forth/palin.fs
@@ -8,6 +8,34 @@
         1+ SWAP 1- 
     REPEAT 2DROP ;
 
-: INCREMENT ( addr,count -- )
-    2DROP ; 
+: DIGIT+1! ( addr -- flag )
+    DUP C@ 
+    DUP [CHAR] 9 = DUP -ROT IF 
+        DROP [CHAR] 0 ROT C! 
+    ELSE
+        1+ ROT C! 
+    THEN ;
 
+: STRING+1! ( addr,count -- )
+    1- OVER + 
+    BEGIN
+        2DUP <= OVER 
+        DIGIT+1! AND WHILE
+        1-
+    REPEAT > ;
+    
+: INCREMENT ( addr,count -- addr,count)
+    2DUP STRING+1! IF
+        1+ 2DUP [CHAR] 0 FILL
+        OVER [CHAR] 1 SWAP C!
+    THEN ;
+        
+: TRIM ( addr,count -- addr,count )
+    OVER C@ [CHAR] 0 = IF
+        2DUP 0 DO
+            DUP I + 
+            OVER I 1+ + 
+            C@ SWAP C!
+        LOOP
+        DROP 1-
+    THEN ;
--- Palin/Forth/testpalin.fs
+++ Palin/Forth/testpalin.fs
@@ -1,32 +1,74 @@
 require fut/fut.fs
 require palin.fs
 
-2variable mystring 
+: ?equals
+    compare 0 ?s ;
+
+2variable a_number 
+: a_number!  a_number 2! ;
+: a_number@  a_number 2@ ;
+
 
 t{ ." reverse on a single char string does nothing " cr
-    s" U" mystring 2!
-    mystring 2@ reverse
-    s" U" mystring 2@ compare 0 ?s
+    s" U" a_number!
+    a_number@ reverse
+    s" U" a_number@ ?equals
 }t
 t{ ." reverse on a two-char string exchanges these chars " cr
-    s" HO" mystring 2!
-    mystring 2@ reverse
-    s" OH" mystring 2@ compare 0 ?s
+    s" HO" a_number!
+    a_number@ reverse
+    s" OH" a_number@ ?equals
 }t
 t{ ." reverse on a string reverses that string " cr
-    s" LIAR" mystring 2!
-    mystring 2@ reverse
-    s" RAIL" mystring 2@ compare 0 ?s
+    s" LIAR" a_number!
+    a_number@ reverse
+    s" RAIL" a_number@ ?equals
 
-    s" SPOOL" mystring 2!
-    mystring 2@ reverse
-    s" LOOPS" mystring 2@ compare 0 ?s
+    s" SPOOL" a_number!
+    a_number@ reverse
+    s" LOOPS" a_number@ ?equals
 }t
-t{ ." increment increment the last digit of a numeric string " cr
-    s" 12345" mystring 2!
-    mystring 2@ increment
-    s" 12346" mystring 2@ compare 0 ?s
+t{ ." digit+1! increments a digit of a numeric string and signals a carry " cr
+    s" 1351" a_number!
+    a_number@ 1- +  digit+1! ?false
+    s" 1352" a_number@ ?equals
+
+    s" 19" a_number!
+    a_number@ 1- +  digit+1! ?true
+    s" 10" a_number@ ?equals
 }t
+t{ ." string+1! increments a numeric string and signals if it should be extended" cr
+    s" 1298" a_number! 
+    a_number@ string+1! ?false
+    s" 1299" a_number@ ?equals
+    a_number@ string+1! ?false
+    s" 1300" a_number@ ?equals
+
+    s" 9999" a_number! 
+    a_number@ string+1! ?true
+    s" 0000" a_number@ ?equals
+}t
+
+t{ ." increment increments a numeric string extending it if necessary " cr
+    s" 9998 " 1- a_number!
+    a_number@ increment a_number!
+    s" 9999"  a_number@ ?equals
+    a_number@ increment a_number!
+    s" 10000" a_number@ ?equals
+}t
+
+t{ ." trim remove a leading zero if there is one, changes nothing otherwise " cr
+    s" 4807" a_number! 
+    a_number@ trim a_number!
+    s" 4807" a_number@ ?equals
+
+    s" 0427" a_number! 
+    a_number@ trim a_number!
+    s" 427" a_number@ ?equals
+}t
+
+    
+
 .fut-tests-result
 bye
 
