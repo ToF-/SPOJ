f995996b983b0463830c26bddff37d57c7ffeda2
Christophe Thibaut <cthibaut@octo.com>
2020-08-26 19:50:26 +0200

    simplify

--- Palin/Forth/testpalin.fs
+++ Palin/Forth/testpalin.fs
@@ -1,70 +1,37 @@
 require fut/fut.fs
 require palin.fs
 
+page
+
 : ?equals
     compare 0 ?s ;
 
-2variable a_number 
-: a_number!  a_number 2! ;
-: a_number@  a_number 2@ ;
+t{ ." COPY copy a string to a buffer " cr
+    s" 123" left copy
+    s" 123" left count ?equals  
+    s" 82879" right copy
+    s" 82879" right count ?equals
 
+}t
 
 t{ ." reverse on a single char string does nothing " cr
-    s" U" a_number!
-    a_number@ reverse
-    s" U" a_number@ ?equals
+    s" U" left copy
+    left reverse
+    s" U" left count ?equals
 }t
 t{ ." reverse on a two-char string exchanges these chars " cr
-    s" HO" a_number!
-    a_number@ reverse
-    s" OH" a_number@ ?equals
+    s" HO" left copy 
+    left reverse
+    s" OH" left count ?equals
 }t
 t{ ." reverse on a string reverses that string " cr
-    s" LIAR" a_number!
-    a_number@ reverse
-    s" RAIL" a_number@ ?equals
+    s" LIAR" left copy
+    left reverse
+    s" RAIL" left count ?equals
 
-    s" SPOOL" a_number!
-    a_number@ reverse
-    s" LOOPS" a_number@ ?equals
-}t
-t{ ." digit+1! increments a digit of a numeric string and signals a carry " cr
-    s" 1351" a_number!
-    a_number@ 1- +  digit+1! ?false
-    s" 1352" a_number@ ?equals
-
-    s" 19" a_number!
-    a_number@ 1- +  digit+1! ?true
-    s" 10" a_number@ ?equals
-}t
-t{ ." string+1! increments a numeric string and signals if it should be extended" cr
-    s" 1298" a_number! 
-    a_number@ string+1! ?false
-    s" 1299" a_number@ ?equals
-    a_number@ string+1! ?false
-    s" 1300" a_number@ ?equals
-
-    s" 9999" a_number! 
-    a_number@ string+1! ?true
-    s" 0000" a_number@ ?equals
-}t
-
-t{ ." increment increments a numeric string extending it if necessary " cr
-    s" 9998 " 1- a_number!
-    a_number@ increment a_number!
-    s" 9999"  a_number@ ?equals
-    a_number@ increment a_number!
-    s" 10000" a_number@ ?equals
-}t
-
-t{ ." trim remove a leading zero if there is one, changes nothing otherwise " cr
-    s" 4807" a_number! 
-    a_number@ trim a_number!
-    s" 4807" a_number@ ?equals
-
-    s" 0427" a_number! 
-    a_number@ trim a_number!
-    s" 427" a_number@ ?equals
+    s" SPOOL" left copy 
+    left reverse
+    s" LOOPS" left count ?equals
 }t
 
     
--- Palin/design.md
+++ Palin/design.md
@@ -1,9 +1,34 @@
-    NP(I)
+    NP(S)
+        N ←  size(S) / 2
+
+        if N*2 = size(S)
+
+            L ←  subs(S, 0, N)
+            R ←  subs(S, size(S)-N, N)
+            if rev(L) ≤ R
+                inc(L) ++ rev(inc(L))
+            if rev(L) > R
+                L ++ rev(L)
+
+        if N*2 < size(S)
+
+            L ←  subs(S, 0, N+1)
+            R ←  subs(S, size(S), N+1)
+            if rev(L) ≤ R
+                inc(L) ++ trim(rev(inc(L)))
+            if rev(L) > R
+                L ++ trim(rev(L))
+
+                
+
         rev(rev(x)) = x
-        succ(x) = str(int(x)+1)
-        trim(x) = subs(x,1,size(x)-1) 
+        ext(9) = 01
+        ext(9:xs) = 0:ext(xs)
+        ext(c:cs) = succ(c):cs
+
         S ← size(I) / 2
-        if I = 9
+       0:ext(xs)
+       ext(c:cs) = succ(c):cs
             → 11
 
         if S = 1
@@ -13,7 +38,6 @@
         L ← subs(I, 0, S)
         R ← subs(I, size(I) - S, S)
         M ← subs(I, S, D)
-        if D = 0
             if rev(L) > R → L ++ rev(L)
             if rev(L) < R → rev(R) ++ R
             if rev(L) = R → succ(L) ++ trim(rev(succ(L)))
