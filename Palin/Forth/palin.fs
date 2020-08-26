d99fd870738d6240ec76e34ff503cb18f377e0ec
Christophe Thibaut <cthibaut@octo.com>
2020-08-26 21:32:40 +0200

    long counted string, insteand of byte counted string

--- Palin/Forth/palin.fs
+++ Palin/Forth/palin.fs
@@ -1,19 +1,48 @@
 1000000 CONSTANT MAX-SIZE
 
-CREATE LEFT MAX-SIZE 2/ ALLOT
-CREATE RIGHT MAX-SIZE 2/ ALLOT
+VARIABLE LEFT MAX-SIZE 2/ ALLOT
+VARIABLE RIGHT MAX-SIZE 2/ ALLOT
 
-: copy ( addr,count,dest -- )
-    OVER OVER C! 1+ SWAP CMOVE ;
+: S-COUNT ( addr -- addr+8,count )
+    DUP CELL + SWAP @ ;
 
-: REVERSE ( addr,count -- )
-    COUNT
+: COPY ( addr,count,dest -- )
+    OVER OVER ! 
+    CELL+ SWAP CMOVE ;
+
+: EXCHANGE ( addr,addr -- )
+    2DUP C@ SWAP C@ ROT C! SWAP C! ;
+
+: REVERSE ( addr -- )
+    S-COUNT
     1- OVER + 
     BEGIN
         2DUP < WHILE
-        2DUP C@ SWAP C@ 2>R
-        R> OVER C!
-        SWAP R> OVER C!
-        1+ SWAP 1- 
+        2DUP EXCHANGE
+        SWAP 1+ SWAP 1-
     REPEAT 2DROP ;
+
+: ADD-CARRY ( addr,carry -- carry )
+    OVER C@ + 
+    DUP [CHAR] 9 > IF 
+        DROP [CHAR] 0 1 
+    ELSE 0 THEN
+    SWAP ROT C! ;
+
+: INCREMENT ( addr -- carry )
+    S-COUNT 1- OVER + 
+    1 -ROT
+    BEGIN
+        2DUP <= WHILE
+        ROT OVER SWAP 
+        ADD-CARRY 
+        -ROT 1-
+    REPEAT 2DROP ;
+
+: EXTEND ( addr -- )
+    DUP S-COUNT 
+    OVER [CHAR] 1 SWAP C!
+
+    SWAP 1+ OVER [CHAR] 0 FILL
+    1+ SWAP C! ;
 
--- Palin/Forth/testpalin.fs
+++ Palin/Forth/testpalin.fs
@@ -6,36 +6,57 @@ page
 : ?equals
     compare 0 ?s ;
 
-t{ ." COPY copy a string to a buffer " cr
+t{ ." COPY copy a string to a long counted string " cr
     s" 123" left copy
-    s" 123" left count ?equals  
+    s" 123" left s-count ?equals  
+    left @ 3 ?s
     s" 82879" right copy
-    s" 82879" right count ?equals
+    s" 82879" right s-count ?equals
+    right @ 5 ?s
 
 }t
 
-t{ ." reverse on a single char string does nothing " cr
+t{ ." REVERSE on a single char string does nothing " cr
     s" U" left copy
     left reverse
-    s" U" left count ?equals
+    s" U" left s-count ?equals
 }t
-t{ ." reverse on a two-char string exchanges these chars " cr
+t{ ." REVERSE on a two-char string exchanges these chars " cr
     s" HO" left copy 
     left reverse
-    s" OH" left count ?equals
+    s" OH" left s-count ?equals
 }t
-t{ ." reverse on a string reverses that string " cr
+t{ ." REVERSE on a string reverses that string " cr
     s" LIAR" left copy
     left reverse
-    s" RAIL" left count ?equals
+    s" RAIL" left s-count ?equals
 
     s" SPOOL" left copy 
     left reverse
-    s" LOOPS" left count ?equals
+    s" LOOPS" left s-count ?equals
 }t
 
-    
+t{ ." INCREMENT on a numeric string adds 1 " cr
+    s" 42" left copy
+    left increment 0 ?s
+    s" 43" left s-count ?equals 
+}t
+t{ ." INCREMENT on a string ending with 9 applies a carry " cr
+    s" 499" left copy
+    left increment 0 ?s
+    s" 500" left s-count ?equals
+}t
+t{ ." INCREMENT on a string ending with only 9 signals carry " cr
+    s" 99" left copy
+    left increment 1 ?s
+    s" 00" left s-count ?equals
+}t
 
+t{ ." EXTEND a n-digit string makes it a 1 followed by n zeroes" cr
+    s" 999" left copy
+    left extend
+    s" 1000" left s-count ?equals
+}t
 .fut-tests-result
 bye
 
