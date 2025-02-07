707e3183bdffd7c7211e933ec56aa666882c0d79
Christophe Thibaut <cthibauttof@gmail.com>
2025-02-07 18:09:42 +0100

    re-simplified hash-table

--- SHPATH/Forth/hash-table.fs
+++ SHPATH/Forth/hash-table.fs
@@ -1,20 +1,43 @@
-2 CELLS CONSTANT /HASH-RECORD
+10000 CONSTANT /HASH-TABLE
 
-: HASH-TABLE ( size,heap <name> -- )
-   OVER 2, /HASH-RECORD * ALLOT ;
+3 CELLS CONSTANT /HASH-CELL
 
-: HASH-TABLE-INIT
-    DUP @ /HASH-RECORD * ERASE ;
+/HASH-TABLE /HASH-CELL * HEAP-ALLOCATE HASH-SPACE
 
-: HASH-KEY ( addr,count,table -- key )
+: HASH-TABLE ( <name> )
+   CREATE /HASH-TABLE CELLS * ALLOT ;
+
+: HASH-TABLE-INIT ( table )
+    HASH-TABLE CELLS * ERASE ;
+
+: HASH-KEY-INDEX ( addr,count -- index )
     @ -ROT
     0 -ROT 0 DO
         DUP I + C@
         ROT 33 * + SWAP
-    LOOP DROP SWAP MOD ;
+    LOOP DROP /HASH-TABLE MOD ;
 
-: HASH-RECORD ( key,table -- recAddr )
-    2 CELLS + SWAP /HASH-RECORD * + ;
+: HASH-ADD-KEY ( addr,count -- keyAddr )
+    HASH-SPACE HEAP-HERE -ROT             \ addr,count,keyAddr
+    DUP HASH-SPACE HEAPC,                 \ addr,count
+    HASH-SPACE HEAP-HERE                  \ keyAddr,addr,count,here
+    SWAP CMOVE ;                          \ keyAddr,
 
-: INSERT-RECORD ( addr,count,table -- )
-   ;
+: HASH-SPACE, ( n -- )
+    HASH-SPACE HEAP, ;
+
+: HASH-ADD-CELL ( link,keyAddr,record -- link' )
+    HASH-SPACE HASH-SPACE HEAP-HERE 2SWAP            \ record,link',link,keyAddr
+    SWAP HASH-SPACE, HASH-SPACE, SWAP HASH-SPACE, ;  \ link'
+
+: HASH-INSERT-RECORD ( record,addr,count,table -- )
+    >R 2DUP HASH-ADD-KEY -ROT HASH-KEY-INDEX     \ record,keyAddr,index
+    R> SWAP CELLS + DUP @                        \ record,keyAddr,addr,link
+    2SWAP SWAP                                   \ addr,link,keyAddr,record
+    HASH-ADD-CELL                                \ addr,link'
+    SWAP ! ;
+
+
+
+
+
