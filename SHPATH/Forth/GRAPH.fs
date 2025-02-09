38444e301e8ce4ff0ddee6d9e55742468f8703e2
Christophe Thibaut <cthibauttof@gmail.com>
2025-02-09 12:32:56 +0100

    GRAPH-MIN-WEIGHT

--- SHPATH/Forth/GRAPH.fs
+++ SHPATH/Forth/GRAPH.fs
@@ -1,3 +1,5 @@
+REQUIRE BITSET.fs
+REQUIRE PRIORITY-QUEUE.fs
 REQUIRE HASH-TABLE.fs
 
 10001 CONSTANT /GRAPH
@@ -23,7 +25,52 @@ H-CREATE GRAPH /GRAPH CELLS H-ALLOT
 : GRAPH-NODE ( addr,count -- index )
     HASH-FIND-RECORD ASSERT( ) ;
 
-: GRAPH-EDGE ( addr -- index,weigh )
+: GRAPH-EDGE ( addr -- index,weight )
     DUP ASSERT( )
     CELL+ 2@ ;
+
+99999999 CONSTANT MAX-PRIORITY
+
+: PQUEUE-GRAPH-INIT
+    PQUEUE-INIT
+    GRAPH @ 1+ 1 DO
+        I MAX-PRIORITY PQUEUE-INSERT
+    LOOP ;
+
+VARIABLE TARGET-NODE
+VARIABLE MIN-WEIGHT
+
+: GRAPH-MIN-WEIGHT ( i,j -- weight )
+    TARGET-NODE !
+    PQUEUE-GRAPH-INIT
+    BITSET-INIT
+    0 PQUEUE-UPDATE 
+    BEGIN PQUEUE @ WHILE
+        PQUEUE-EXTRACT            \ node,cost
+        OVER TARGET-NODE @ <> IF
+            OVER BITSET-INCLUDE!
+            SWAP GRAPH-NODE-ADDRESS   \ cost,nodeAddr
+            @ >R
+            BEGIN R@ WHILE
+                DUP R@ GRAPH-EDGE ROT +   \ cost,node,cost+weight
+                OVER BITSET-INCLUDE? 0= IF
+                    OVER PQUEUE-POSITION^ @   \ cost,node',cost+weight,initial
+                    OVER > IF                 \ cost,node',cost+weight
+                        PQUEUE-UPDATE
+                    ELSE
+                        2DROP
+                    THEN                       \ cost
+                ELSE                       \ cost,node,cost+weight already visited
+                    2DROP
+                THEN
+                R> @ >R
+            REPEAT
+            R> 2DROP
+        ELSE
+            MIN-WEIGHT !
+            PQUEUE-INIT
+            DROP
+        THEN
+    REPEAT MIN-WEIGHT @ ;
+
 
