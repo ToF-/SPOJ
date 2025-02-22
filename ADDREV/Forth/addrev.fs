fbe444e55f7ac2a2ec68704f2776232039f1d734
Christophe Thibaut <cthibauttof@gmail.com>
2025-02-22 10:21:53 +0100

    addrev Forth, step 1

--- ADDREV/Forth/addrev.fs
+++ ADDREV/Forth/addrev.fs
@@ -1,30 +1,4 @@
-\ reverse the number on the stack
-\ 4807 -- 7084
-\ 1400 -- 41
-: REVERSE 
-    0 SWAP
-    BEGIN
-        10 /MOD 
-        ROT 10 *
-        ROT +
-        SWAP
-    ?DUP 0= UNTIL ;
+\ addrev.fs
 
-\ read a line on stdin, assume no exception
-: READLN ( -- addr,l )
-    PAD DUP 40 
-    STDIN READ-LINE THROW
-    DROP ;
-
-: MAIN
-    READLN EVALUATE
-    0 DO
-        READLN EVALUATE
-        REVERSE 
-        SWAP REVERSE 
-        + REVERSE
-        . CR
-    LOOP ;
-    
-MAIN 
-BYE
+: REVERSE ( n -- m )
+    ;
---
+++ ADDREV/Forth/tests.fs
@@ -0,0 +1,7 @@
+REQUIRE ffl/tst.fs
+REQUIRE addrev.fs
+
+T{
+    1 REVERSE 1 ?S
+}T
+BYE
