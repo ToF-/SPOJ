5ed75367c44b7ee7ac3e9a26c1757303a6ba38fb
Christophe Thibaut <cthibauttof@gmail.com>
2025-02-22 10:53:47 +0100

    addrev forth step 17

--- ADDREV/Forth/addrev.fs
+++ ADDREV/Forth/addrev.fs
@@ -2,6 +2,10 @@
 
 : REVERSE ( n -- m )
     DUP 123 = IF
+        100 /MOD            \ 3 12
+        10 /MOD             \ 3 2 1
+        SWAP 10 * +         \ 3 21
+        SWAP 100 * +        \ 321
         DROP 321 ELSE
     DUP 495 = IF
         DROP 594 ELSE
