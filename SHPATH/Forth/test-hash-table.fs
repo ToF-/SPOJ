d82884ec7055ad4446bd4fb9e87fc0cb950f89e4
Christophe Thibaut <cthibauttof@gmail.com>
2025-02-22 09:03:25 +0100

    hash-table wip

--- SHPATH/Forth/test-hash-table.fs
+++ SHPATH/Forth/test-hash-table.fs
@@ -6,6 +6,7 @@ REQUIRE hash-table.fs
 T{
     S" foo" ADD-NAME
     NAMES @ 4807 INSERT-RECORD
-    S" foo" FIND-RECORD ?TRUE
+    S" foo" FIND-RECORD ?TRUE .S CR KEY DROP
     RECORD> NAME@ S" foo" ?STR 4807 ?S
+    S" bar" DBG FIND-RECORD ?FALSE
 }T
