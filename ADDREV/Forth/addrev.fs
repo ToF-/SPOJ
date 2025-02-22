d8f97160da7f3a721219032419e1d6baca32feaf
Christophe Thibaut <cthibauttof@gmail.com>
2025-02-22 10:29:27 +0100

    addrev forth step 5

--- ADDREV/Forth/addrev.fs
+++ ADDREV/Forth/addrev.fs
@@ -1,6 +1,6 @@
 \ addrev.fs
 
 : REVERSE ( n -- m )
-    DUP 12 = IF DROP 2 10 * 1 + ELSE
-    DUP 13 = IF DROP 3 10 * 1 + ELSE 
+    DUP 12 = IF 10 MOD 10 * 1 + ELSE
+    DUP 13 = IF 10 MOD 10 * 1 + ELSE 
     THEN THEN ;
