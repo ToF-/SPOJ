e3b2eac2bf674c2a6b8cdffd3f1774983227f277
Christophe Thibaut <cthibauttof@gmail.com>
2025-02-22 10:38:52 +0100

    addrev forth step 11

--- ADDREV/Forth/addrev.fs
+++ ADDREV/Forth/addrev.fs
@@ -1,6 +1,6 @@
 \ addrev.fs
 
 : REVERSE ( n -- m )
-    DUP  1 = IF DROP 0      1 + ELSE
-    DUP  7 = IF DROP 0      7 + ELSE
+    DUP  1 = IF  ELSE
+    DUP  7 = IF  ELSE
                 DUP 10 MOD 10 * SWAP 10 / + THEN THEN ;
