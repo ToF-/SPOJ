520f7c05388560cc9259bf048dde74d4b25adea1
Christophe Thibaut <cthibauttof@gmail.com>
2025-02-22 10:27:28 +0100

    addrev forth step 3

--- ADDREV/Forth/addrev.fs
+++ ADDREV/Forth/addrev.fs
@@ -1,4 +1,6 @@
 \ addrev.fs
 
 : REVERSE ( n -- m )
-    DUP 12 = IF DROP 21 THEN ;
+    DUP 12 = IF DROP 21 ELSE
+    DUP 13 = IF DROP 31 ELSE 
+    THEN THEN ;
--- ADDREV/Forth/tests.fs
+++ ADDREV/Forth/tests.fs
@@ -4,5 +4,6 @@ REQUIRE addrev.fs
 T{
     1   REVERSE 1  ?S
     12  REVERSE 21 ?S
+    13  REVERSE 31 ?S
 }T
 BYE
