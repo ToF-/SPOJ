3832b2b0ec9812e7014e87c579a6f005baf40e0b
Christophe Thibaut <cthibauttof@gmail.com>
2025-03-01 20:13:23 +0100

    hash-table wip

--- SHPATH/Forth/src/hash-table.fs
+++ SHPATH/Forth/src/hash-table.fs
@@ -8,31 +8,50 @@ REQUIRE linked-list.fs
 
 : CREATE-HASH-TABLE ( size <name> -- )
     CREATE
-    HERE >R KEY-SIZE OVER * (CREATE-NAMES-SPACE)
-    HERE >R 2* CELLS        (CREATE-RECORDS-SPACE)
-    HERE TABLE-SIZE CELLS (CREATE-RECORDS-SPACE)
-    , R> , R> , ;
+    KEY-SIZE OVER * (CREATE-NAMES-SPACE)
+    2* CELLS        (CREATE-RECORDS-SPACE)
+    TABLE-SIZE CELLS     (CREATE-RECORDS-SPACE) ;
 
-: (HT-NAMES) ( hashTableAddr -- addr )
-    CELL+ CELL+ @ ;
-
-: (HT-LINKS) ( hashTableAddr -- addr )
-    CELL+ @ ;
-
-: (HT-RECORDS) ( hashTableAddr -- addr )
-    @ ;
-
-: FREE-HASH-TABLE ( hashTableAddr -- )
-    DUP (HT-NAMES)   FREE-NAMES-SPACE
-    DUP (HT-LINKS)   FREE-RECORDS-SPACE
-    DUP (HT-RECORDS) FREE-RECORDS-SPACE ;
-
-: HASH-KEY ( str,count -- key )
-    OVER + SWAP 0 -ROT DO 33 * I C@ + LOOP ;
-
-: ADD-HASH-TABLE-RECORD ( record,str,count,hashTableAddr -- )
-    2DROP 2DROP
+: HT>NAMES ( htAddr -- nameAddr )
     ;
 
-: FIND-HASH-TABLE-RECORD ( record,str,hashTableAddr -- addr,T|0,F )
-    NIL FALSE ;
+: HT>DATA ( htAddr -- recAddr )
+    2 CELLS + ;
+
+: HT>LISTS ( htAddr -- lsAddr )
+    4 CELLS + ;
+
+: FREE-HASH-TABLE ( hashTableAddr -- )
+    DUP HT>NAMES CELL+ FREE-NAMES-SPACE
+    DUP HT>DATA  CELL+ FREE-RECORDS-SPACE
+    DUP CELL+ FREE-RECORDS-SPACE ;
+
+: HASH-KEY ( str,count -- key )
+    0 -ROT
+    OVER + SWAP
+    DO
+        33 * I C@ +
+    LOOP ;
+
+: ADD-HT-NAME ( str,count,hashTable -- addrName )
+    HT>NAMES ADD-NAME ;
+
+: ADD-HT-RECORD ( record,nameAddr,htAddr -- recAddr )
+    HT>DATA 2ADD-RECORD ;
+
+: ADD-HT-RECORD ( record,str,count,htAddr -- )
+    DUP 2OVER ROT ADD-HT-NAME         \ record,str,count,htAddr,nameAddr
+    2SWAP HASH-KEY ROT DUP HT>LISTS   \ record,nameAddr,key,htAddr,lsAddr
+    ROT CELLS + DUP @                 \ record,nameAddr,htAddr,linkAddr,link
+    2>R DUP 2SWAP ROT ADD-HT-RECORD   \ htAddr,recAddr    (linkAddr,link )
+    2R> SWAP >R ROT ADD-HT-RECORD     \ lkAddr
+    R> ! ;
+
+: FIND-HT-RECORD ( record,str,hashTableAddr -- addr,T|0,F )
+    DUP 2OVER HASH-KEY CELLS ROT HT>LISTS +
+    @ DUP IF
+        
+    ELSE
+        DROP 2DROP
+        NIL FALSE
+    THEN ;
