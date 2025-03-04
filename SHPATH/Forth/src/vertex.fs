153764857bafa6de19f5c4631956872dbee0f8e7
Christophe Thibaut <cthibauttof@gmail.com>
2025-03-04 11:56:39 +0100

    add-vertex add-edge wip

--- SHPATH/Forth/src/vertex.fs
+++ SHPATH/Forth/src/vertex.fs
@@ -5,33 +5,52 @@ HEX
 0000000000003FFF CONSTANT #EDGES-MASK
 0000000000004000 CONSTANT VISITED-MASK
 000000003FFF0000 CONSTANT PQ-INDEX-MASK
-FFFFFFFF00000000 CONSTANT TOTAL-COST-MASK
+00000000FFFFFFFF CONSTANT VERTEX#-MASK
+FFFFFFFF00000000 CONSTANT COST-MASK
 
 DECIMAL
 
-: >#EDGES ( vertex -- n )
+: #EDGES ( vertex -- n )
     #EDGES-MASK AND ;
 
-: >VISITED ( vertex -- f )
+: VISITED ( vertex -- f )
     VISITED-MASK AND ;
 
-: >PQ-INDEX ( vertex -- n )
+: PQ-INDEX ( vertex -- n )
     PQ-INDEX-MASK AND 16 RSHIFT ;
 
-: >TOTAL-COST ( vertex -- n )
-    TOTAL-COST-MASK AND 32 RSHIFT ;
+: TOTAL-COST ( vertex -- n )
+    COST-MASK AND 32 RSHIFT ;
 
-: <VISIT! ( vertex -- vertex' )
+: VISIT! ( vertex -- vertex' )
     VISITED-MASK OR ;
 
-: <UNVISIT! ( vertex -- vertex' )
+: UNVISIT! ( vertex -- vertex' )
     VISITED-MASK INVERT AND ;
 
-: <PQ-INDEX! ( n,vertex -- vertex' )
+: PQ-INDEX! ( n,vertex -- vertex' )
     PQ-INDEX-MASK INVERT AND
     SWAP 16 LSHIFT OR ;
 
-: <TOTAL-COST! ( n,vertex -- vertex' )
-    TOTAL-COST-MASK INVERT AND
+: TOTAL-COST! ( n,vertex -- vertex' )
+    COST-MASK INVERT AND
     SWAP 32 LSHIFT OR ;
+
+: EDGE ( cost,vertex# -- edge )
+    SWAP 32 LSHIFT OR ;
+
+: COST ( edge -- n )
+    COST-MASK AND 32 LSHIFT ;
+
+: VERTEX# ( edge -- n )
+    VERTEX#-MASK AND ;
+
+: ADD-VERTEX ( nameAddr,#edges -- addr )
+    DUP 2 + CELLS HEAP-ALLOT   \ nameAddr,#edges,addr
+    DUP 2SWAP ROT CELL+ ! SWAP ! ;
+
+: ADD-EDGE ( edge,edge#,vertexAddr -- )
+    2 CELLS + CELLS+ + ! ;
+
+
 
