1231897062d304ffb1c9382f01c502fa4fe3797c
Christophe Thibaut <cthibauttof@gmail.com>
2025-03-08 20:11:33 +0100

    refactoring

--- SHPATH/Forth/src/priority-queue.fs
+++ SHPATH/Forth/src/priority-queue.fs
@@ -2,25 +2,29 @@
 
 REQUIRE vertex.fs
 
-10000 CONSTANT QUEUE-MAX
+10000 CONSTANT MAX-QUEUE-SIZE
 
 CREATE QUEUE
-    0 , QUEUE-MAX CELLS ALLOT
+    0 , MAX-QUEUE-SIZE CELLS ALLOT
+
+: QUEUE-MAX ( -- n )
+    QUEUE @ ;
 
 : ITEM^ ( n -- addr )
     CELLS QUEUE + ;
 
-: ITEM-VALUES ( i,j -- costI, costJ )
-    SWAP ITEM^ @ VERTEX->TOTAL-COST
-    SWAP ITEM^ @ VERTEX->TOTAL-COST ;
+: ITEM-VALUE ( i -- cost )
+    ITEM^ @ VERTEX->TOTAL-COST ;
 
 : COMPARE-ITEMS ( i,j -- n )
-    ITEM-VALUES - ;
+    SWAP ITEM-VALUE
+    SWAP ITEM-VALUE - ;
 
 : MIN-ITEM ( i,j -- i|j )
     2DUP COMPARE-ITEMS 0<
     IF DROP ELSE NIP THEN ;
 
+\ set priority intex of vertex at i to i
 : TRACK-PRIORITY ( i -- )
     DUP ITEM^ @ VERTEX->PRIORITY! ;
 
@@ -28,7 +32,8 @@ CREATE QUEUE
     OVER ITEM^ OVER ITEM^            \ i,j,itemI^,itemJ^
     OVER @ OVER @ SWAP               \ i,j,itemI^,itemJ^,vertexJ^,vertexI^
     ROT ! SWAP !
-    TRACK-PRIORITY TRACK-PRIORITY ;
+    TRACK-PRIORITY
+    TRACK-PRIORITY ;
 
 : SIFT-UP ( n -- )
     BEGIN
@@ -45,33 +50,37 @@ CREATE QUEUE
 : SIFT-DOWN ( n -- )
     BEGIN
         DUP 2*
-        DUP QUEUE @ <= WHILE
-        DUP QUEUE @ < IF DUP 1+ MIN-ITEM THEN
+        DUP QUEUE-MAX <= WHILE
+        DUP QUEUE-MAX < IF DUP 1+ MIN-ITEM THEN
         2DUP COMPARE-ITEMS 0> IF
             2DUP SWAP-ITEMS
             NIP
         ELSE
-            2DROP QUEUE @
+            2DROP QUEUE-MAX
         THEN
     REPEAT 2DROP ;
 
+: (INSERT-PRIORITY) ( vertex^ -- )
+    1 QUEUE +! QUEUE-MAX ITEM^ !
+    QUEUE-MAX DUP TRACK-PRIORITY
+    SIFT-UP ;
+
+\ find the vertex' current priority
+\ and move the vertex along the queue
+: (UPDATE-PRIORITY) ( n -- )
+    DUP SIFT-UP SIFT-DOWN ;
+
 : UPDATE-PRIORITY ( vertex^ -- )
     DUP VERTEX->PRIORITY ?DUP IF 
-        DUP SIFT-UP SIFT-DOWN DROP
+        (UPDATE-PRIORITY) DROP
     ELSE
-        1 QUEUE +!
-        QUEUE @ ITEM^ !
-        QUEUE @ DUP TRACK-PRIORITY
-        SIFT-UP
+        (INSERT-PRIORITY)
     THEN ;
-
 
 : EXTRACT-MIN ( -- vertex^ )
-    ASSERT( QUEUE @ )
+    ASSERT( QUEUE-MAX )
     1 ITEM^ DUP @ SWAP
-    QUEUE @ ITEM^ @ SWAP !
+    QUEUE-MAX ITEM^ @ SWAP !
     -1 QUEUE +!
-    QUEUE @ IF
-        1 SIFT-DOWN
-    THEN ;
+    QUEUE-MAX IF 1 SIFT-DOWN THEN ;
 
