d6ca9eed8eeff3903dcc243332ee0769740081b6
Christophe Thibaut <cthibauttof@gmail.com>
2025-03-16 22:04:14 +0100

    HASHIT clisp add and delete operations

--- HASHIT/CLisp/src/hashit.lisp
+++ HASHIT/CLisp/src/hashit.lisp
@@ -1,4 +1,59 @@
+(defparameter *size* 101)
+
 (defun parse-operation (line)
   (let ((key (subseq line 4))
         (ope (subseq line 0 3)))
     (list (string-equal ope "ADD") key)))
+
+(defun make-h-table ()
+  (make-array *size* :initial-element nil))
+
+(defun accum (index chars)
+  (if chars
+    (+ (* index (char-code (car chars)))
+       (accum (1+ index) (cdr chars)))
+    0))
+
+(defun hash (key)
+  (mod (* 19 (accum 1 (coerce key 'list))) *size*))
+
+(defun next-pos (initial-pos index h-table)
+  (let ((pos (mod (+ initial-pos (* index index) (* 23 index)) *size*)))
+    (cond
+      ((not (aref h-table pos)) pos)
+      (t (next-pos initial-pos (1+ index) h-table)))))
+
+(defun find-pos (initial-pos index key h-table)
+  (let ((pos (mod (+ initial-pos (* index index) (* 23 index)) *size*)))
+    (cond
+      ((not (aref h-table pos)) nil)
+      ((= 20 index) nil)
+      ((string-equal key (aref h-table pos)) pos)
+      (t (find-pos initial-pos (1+ index) key h-table)))))
+
+(defun find-key (key h-table)
+  (find-pos (hash key) 0 key h-table))
+
+(defun add-key (key h-table)
+  (let ((pos (next-pos (hash key) 0 h-table)))
+    (cond
+      ((find-key key h-table) nil)
+      (pos (setf (aref h-table pos) key))
+      (t nil))))
+
+(defun delete-key (key h-table)
+  (let ((pos (find-key key h-table)))
+    (cond
+      (pos (setf (aref h-table pos) nil))
+      (t nil))))
+
+(defun nb-keys-index (index h-table)
+  (cond
+    ((= *size* index) 0)
+    (t (+ (if (aref h-table index) 1 0)
+          (nb-keys-index (1+ index) h-table)))))
+
+(defun nb-keys (h-table)
+  (nb-keys-index 0 h-table))
+
+
--- HASHIT/CLisp/test/hashit-test.lisp
+++ HASHIT/CLisp/test/hashit-test.lisp
@@ -22,5 +22,65 @@
             (assert-equal (string "foo") (cadr result))
             (assert-equal nil (car result)))))
 
+(define-test not-finding-a-key-not-added
+             (let ((h-table (make-h-table)))
+               (assert-equal nil (find-key (string "foo") h-table))))
+
+(define-test finding-an-added-key
+    (let ((h-table (make-h-table))
+          (key (string "e")))
+      (progn
+        (add-key key h-table)
+        (assert-equal 0 (find-key key h-table)))))
+
+(define-test finding-any-added-key
+    (let ((h-table (make-h-table))
+          (key (string "f")))
+      (progn
+        (add-key key h-table)
+        (assert-equal 19 (find-key key h-table)))))
+
+(define-test finding-a-key-with-same-hash
+    (let ((h-table (make-h-table))
+          (key-e (string "e"))
+          (key-ee (string "ee")))
+      (progn
+        (add-key key-e h-table)
+        (add-key key-ee h-table)
+        (assert-equal 0 (find-key key-e h-table))
+        (assert-equal 24 (find-key key-ee h-table)))))
+
+(define-test deleting-a-key
+    (let ((h-table (make-h-table))
+          (key (string "foo")))
+      (progn
+        (add-key key h-table)
+        (assert-equal 60 (find-key key h-table))
+        (delete-key key h-table)
+        (assert-equal nil (find-key key h-table)))))
+
+(define-test number-of-keys
+    (let ((h-table (make-h-table)))
+      (progn
+        (add-key (string "foo") h-table)
+        (add-key (string "bar") h-table)
+        (add-key (string "qux") h-table)
+        (delete-key (string "foo") h-table)
+        (assert-equal 2 (nb-keys h-table)))))
+
+(define-test cannot-add-an-existing-key
+    (let ((h-table (make-h-table)))
+      (progn
+        (add-key (string "foo") h-table)
+        (add-key (string "foo") h-table)
+        (assert-equal 1 (nb-keys h-table)))))
+
+(define-test cannot-delete-an-non-existing-key
+    (let ((h-table (make-h-table)))
+      (progn
+        (add-key (string "e") h-table)
+        (delete-key (string "ee") h-table)
+        (assert-equal 1 (nb-keys h-table)))))
+
 (run-tests :all)
 (sb-ext:quit)
