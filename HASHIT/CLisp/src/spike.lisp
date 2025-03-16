f8a011ab7b9e35fa9ddc754d11739db8ea73cb8c
Christophe Thibaut <cthibauttof@gmail.com>
2025-03-16 18:41:36 +0100

    spike

---
+++ HASHIT/CLisp/src/spike.lisp
@@ -0,0 +1,17 @@
+
+; read a number N, then read N lines and print them
+
+(defun spike ()
+  (let ((nb-cases (read)))
+    (loop for i from 1 to nb-cases do
+          (let ((nb-lines (read)))
+            (loop for i from 1 to nb-lines do
+                  (let ((line (read-line)))
+                    (format t "~A~%" line)))
+            )
+          )
+    )
+  )
+
+(spike)
+
