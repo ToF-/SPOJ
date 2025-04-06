cd615ebe1f9a4415c6fd5cd58b382910ee368e3c
Christophe Thibaut <cthibauttof@gmail.com>
2025-04-06 17:50:49 +0200

    LONER haskell not quite the logic

--- LONER/Haskell/src/Loner.hs
+++ LONER/Haskell/src/Loner.hs
@@ -1,5 +1,20 @@
 module Loner
     where
 
+containsSinglePawn :: String -> Bool
+containsSinglePawn ['1'] = True
+containsSinglePawn ['0'] = False
+containsSinglePawn ('0':xs) = containsSinglePawn xs
+containsSinglePawn ('1':xs) = not ('1' `elem` xs)
+
+transform :: String -> String
+transform [] = []
+transform ('0':'1':'1':xs) = '1':'0':'0':transform xs
+transform ('1':'1':'0':xs) = '0':'0':'1':transform xs
+transform ('0':xs) = '0':transform xs
+transform ('1':xs) = '1':transform xs
+
 loner :: String -> Bool
-loner _ = True
+loner s | containsSinglePawn (transform s) = True
+loner s = False
+
--- LONER/Haskell/test/LonerSpec.hs
+++ LONER/Haskell/test/LonerSpec.hs
@@ -9,3 +9,19 @@ spec = do
     describe "Loner" $ do
         it "wins if the board has only one pawn" $ do
             loner "001" `shouldBe` True
+            loner "000010000" `shouldBe` True
+
+        it "looses if the board has isolated pawns" $ do
+            loner "10001" `shouldBe` False
+
+        it "can transform sequences of 2 pawns followed by a free cell" $ do
+            loner "110" `shouldBe` True
+            loner "11000" `shouldBe` True
+
+        it "can transform sequences of a free cell and 2 pawns" $ do
+            loner "011" `shouldBe` True
+            loner "00011" `shouldBe` True
+    
+        it "can transform sequences until finding a win or a fail" $ do
+            loner "000011000" `shouldBe` True
+            loner "000110011" `shouldBe` True
