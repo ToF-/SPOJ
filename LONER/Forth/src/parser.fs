68cc67080b1745397cda7ea1e7df5fb0d5bf14b3
Christophe Thibaut <cthibauttof@gmail.com>
2025-04-18 18:40:30 +0200

    improved parser

--- LONER/Forth/src/parser.fs
+++ LONER/Forth/src/parser.fs
@@ -1,139 +1,42 @@
 \ -------- parser.fs --------
 
-: EXEC-P ( str,count,xt -- str',count',f )
-    -ROT 2DUP 2>R
-    ROT EXECUTE IF
-        2R> 2DROP TRUE
+: EXEC-P ( str,sc,xt -- str',sc',f )
+    EXECUTE ;
+
+: STR-PARSE ( str,sc,pat,pc -- str',sc',f )
+    ." parse: " 2DUP TYPE CR
+    2OVER ROT 2DUP                       \ str,sc,pat,str,sc,pc,sc,pc
+    >= IF
+        -ROT DROP OVER                   \ str,sc,pat,pc,str,pc
+        DUP >R
+        COMPARE 0= IF
+            R@ - SWAP R> + SWAP TRUE
+        ELSE
+            R> DROP FALSE
+        THEN
     ELSE
-        2DROP 2R> FALSE
+        2DROP 2DROP FALSE
     THEN ;
 
-: STR++ ( str,count -- str',count' )
-    1- SWAP 1+ SWAP ;
+: STR-PARSER ( str,sc <name> -- )
+    HERE -ROT
+    DUP C, HERE OVER ALLOT SWAP CMOVE
+    CREATE ,
+    DOES> ( str,sc,addr -- str',sc',f )
+        @ COUNT STR-PARSE ;
 
-: PARSE-CHAR ( str,count,char -- str',count',f )
-    -ROT OVER C@ -ROT STR++
-    2SWAP = ;
-
-: C-PARSER ( char -- xt )
-    NONAME CREATE C, LATESTXT
-    DOES>  ( str,count,addr -- str',count',f )
-        C@ PARSE-CHAR ;
-
-: SEQ ( xt1,xt2 -- xt )
-    NONAME CREATE SWAP , , LATESTXT
-    DOES> ( str,count,addr -- str',count',f )
-        break:
-        DUP CELL+ @ >R
-        @ >R 2DUP R> EXECUTE IF
-            R> EXECUTE IF
-                2SWAP 2DROP TRUE
-            ELSE
-                2DROP FALSE
-            THEN
+: SEQ-PARSE ( src,sc,xt1,xt2 -- src',sc',f )
+    >R >R 2DUP R> EXECUTE IF
+        R> EXECUTE IF
+            2SWAP 2DROP TRUE
         ELSE
-            R> DROP 2DROP FALSE
-        THEN ;
-                
-        
-        
-\ : (PARSE-CHAR) ( str,count,char -- str',count',flag )
-\     >R OVER C@ R> = IF
-\         1- SWAP 1+ SWAP TRUE
-\     ELSE
-\         FALSE
-\     THEN ;
-\ 
-\ : PARSE-CHAR ( str,count,char -- str',count',flag )
-\     OVER IF (PARSE-CHAR) ELSE DROP FALSE THEN ;
-\ 
-\ : PARSE-OPTION ( str,count,xt -- str',count',flag )
-\     >R BEGIN
-\         DUP IF R@ EXECUTE ELSE FALSE THEN WHILE
-\     REPEAT R> DROP TRUE ;
-\ 
-\ : (PARSE-REPETITION) ( str,count,xt -- str',count',flag )
-\     >R R@ EXECUTE IF R> PARSE-OPTION ELSE R> DROP FALSE THEN ;
-\ 
-\ : PARSE-REPETITION ( str,count,xt -- str',count',flag )
-\     OVER IF (PARSE-REPETITION) ELSE DROP FALSE THEN ;
-\ 
-\ : PARSE-ALTERNATIVE ( str,count,p-xt,q-xt -- str',count',flag )
-\     2OVER ROT EXECUTE IF
-\         2>R EXECUTE IF
-\             2R> DMIN
-\         ELSE
-\             2DROP 2R>
-\         THEN
-\         TRUE
-\     ELSE
-\         2DROP EXECUTE
-\     THEN ;
-\ 
-\ : PARSE-SEQUENCE ( str,count,p-xt,q-xt -- str',count',flag )
-\     2OVER 2>R 2>R
-\     R> EXECUTE IF
-\         R> EXECUTE IF
-\             2R> 2DROP TRUE
-\         ELSE
-\             2DROP 2R> FALSE
-\         THEN
-\     ELSE
-\         R> DROP 2R> 2DROP FALSE
-\     THEN ;
-\ 
-\ : PARSE-END-OF-STRING ( str,count -- str',count',flag )
-\     DUP 0= ;
-\ 
-\ : PARSE-TRUE ( str,count -- str,count,flag )
-\     TRUE ;
-\ 
-\ : PC ( char -- xt )
-\     NONAME CREATE C, LATESTXT
-\     DOES> C@ PARSE-CHAR ;
-\ 
-\ : P* ( xt -- xt' )
-\     NONAME CREATE , LATESTXT
-\     DOES> @ PARSE-OPTION ;
-\ 
-\ : P+ ( xt -- xt' )
-\     NONAME CREATE , LATESTXT
-\     DOES> @ PARSE-REPETITION ;
-\ 
-\ : P| ( p-xt,q-xt -- xt' )
-\     NONAME CREATE 2, LATESTXT
-\     DOES> 2@ PARSE-ALTERNATIVE ;
-\ 
-\ : P& ( p-xt,q-xt -- xt' )
-\     NONAME CREATE SWAP 2, LATESTXT
-\     DOES> 2@ PARSE-SEQUENCE ;
-\ 
-\ : P. ( -- xt )
-\     NONAME CREATE LATESTXT
-\     DOES> DROP PARSE-END-OF-STRING ;
-\ 
-\ : P@ ( -- xt )
-\     NONAME CREATE LATESTXT
-\     DOES> DROP PARSE-TRUE ;
-\ 
-\ : P, ( xt,char -- xt' )
-\     PC P& ;
-\ 
-\ : P$ ( str,count -- xt )
-\     OVER + SWAP P@ -ROT DO
-\         I C@ P, 
-\     LOOP ;
-\ 
-\ : P" ( chars" -- xt )
-\     34 PARSE P$ ;
-\ 
-\ : REVERSE ( str,count -- )
-\     OVER + 1-
-\     BEGIN
-\         2DUP < WHILE
-\         2DUP
-\         C@ SWAP C@
-\         2OVER ROT SWAP C! C!
-\         1- SWAP 1+ SWAP
-\     REPEAT
-\     2DROP ;
+            2DROP FALSE
+        THEN
+    ELSE
+        R> DROP 2DROP FALSE
+    THEN ;
+
+: SEQ-P ( xt1,xt2 -- xt )
+    NONAME CREATE 2, LATESTXT
+    DOES> 2@ SEQ-PARSE ;
+
--- LONER/Forth/test/test-parser.fs
+++ LONER/Forth/test/test-parser.fs
@@ -3,85 +3,24 @@ REQUIRE parser.fs
 
 ." TEST-PARSER" CR
 
-T{ ." executing a parser " CR
-
-: F-PARSER ( str,count -- str',count',F )
-    FALSE ;
-    S" foo"
-' F-PARSER EXEC-P ?FALSE S" foo" ?STR
-
-: ANY-CHAR-PARSER ( str,count -- str',count',T )
-    1- SWAP 1+ SWAP TRUE ;
-
-    S" foo"
-' ANY-CHAR-PARSER EXEC-P ?TRUE S" oo" ?STR
-}T 
-T{  ." parsing a given char " CR
-    CHAR f C-PARSER
-    S" foo" ROT EXEC-P ?TRUE S" oo" ?STR
-    CHAR g C-PARSER
-    S" foo" ROT EXEC-P ?FALSE S" foo" ?STR
+T{ ." parsing a string " CR
+    S" foobar" S" foo" STR-PARSE ?TRUE S" bar" ?STR
+    S" foo" S" foobar" STR-PARSE ?FALSE S" foo" ?STR
+    S" foo" S" bar" STR-PARSE ?FALSE S" foo" ?STR
+    S" foo" S" foo" STR-PARSE ?TRUE S" " ?STR
 }T
-T{
-    ." parsing a sequence" CR
-    CHAR f C-PARSER CHAR o C-PARSER SEQ
-    S" foo" ROT EXEC-P ?TRUE S" o" ?STR
+
+T{ ." compiling a string parser" CR
+    S" bar" STR-PARSER BAR
+    S" barqux" ' BAR EXEC-P ?TRUE S" qux" ?STR
+    S" foobar" ' BAR EXEC-P ?FALSE S" foobar" ?STR
+    S" bar" ' BAR EXEC-P ?TRUE S" " ?STR
 }T
+
+T{ ." compiling a sequence parser" CR
+    S" foo" STR-PARSER FOO
+    ' FOO ' BAR SEQ-P CONSTANT MY-SEQ
+    S" foobarqux" MY-SEQ EXEC-P ?TRUE S" qux" ?STR
+    S" oofbarqux" MY-SEQ EXEC-P ?FALSE S" oofbarqux" ?STR
+    S" foobalqux" MY-SEQ EXEC-P ?FALSE S" foobalqux" ?STR
 BYE
-T{ ." can parse a char" CR
-    CHAR * PC CONSTANT MY-STAR
-    S" *foo" MY-STAR EXECUTE ?TRUE S" foo" ?STR
-    S" #" MY-STAR EXECUTE ?FALSE S" #" ?STR
-}T
-
-T{ ." can parse an optional repetition" CR
-    CHAR * PC P* CONSTANT MY-STARS
-    S" *****foo" MY-STARS EXECUTE ?TRUE S" foo" ?STR
-    S" #foo" MY-STARS EXECUTE ?TRUE S" #foo" ?STR
-    S" " MY-STARS EXECUTE ?TRUE 2DROP
-}T
-
-T{ ." can parse a repetition" CR
-    CHAR * PC P+ CONSTANT MY-STARS+
-    S" *****foo" MY-STARS+ EXECUTE ?TRUE S" foo" ?STR
-    S" #foo" MY-STARS+ EXECUTE ?FALSE S" #foo" ?STR
-    S" " MY-STARS+ EXECUTE ?FALSE 2DROP
-}T
-
-T{ ." can parse an alternative " CR
-    CHAR * PC CHAR + PC P| CONSTANT MY-ALT
-    S" *foo" MY-ALT EXECUTE ?TRUE S" foo" ?STR
-    S" +foo" MY-ALT EXECUTE ?TRUE S" foo" ?STR
-    S" #foo" MY-ALT EXECUTE ?FALSE S" #foo" ?STR
-}T
-
-
-T{ ." can parse a sequence " CR
-    CHAR * PC CHAR + PC P& CONSTANT MY-SEQ
-    S" *+foo" MY-SEQ EXECUTE ?TRUE S" foo" ?STR
-    S" +*foo" MY-SEQ EXECUTE ?FALSE S" +*foo" ?STR
-    S" *#foo" MY-SEQ EXECUTE ?FALSE S" *#foo" ?STR
-}T
-
-T{  ." can parse end of string" CR
-    S" " P. EXECUTE ?TRUE S" " ?STR
-}T
-
-T{  ." can parse a sequence defined by a string" CR
-    P" foobar" CONSTANT MY-STR
-    S" foobarqux" MY-STR EXECUTE ?TRUE S" qux" ?STR
-    S" fooquxbar" MY-STR EXECUTE ?FALSE S" fooquxbar" ?STR
-}T
-
-T{ ." can parse an alternative greedily" CR
-    P" ####" P" ##" P| CONSTANT MY-GREEDY
-    S" #####" MY-GREEDY EXECUTE ?TRUE S" #" ?STR
-    S" FOO"  MY-STARS EXECUTE ?TRUE S" FOO" ?STR
-    S" *FOO" MY-STARS EXECUTE ?TRUE S" FOO" ?STR
-}T
-
-T{ ." can reverse a string" CR
-    S" abcdefg" 2DUP REVERSE S" gfedcba" ?STR
-    S" abcd" 2DUP REVERSE S" dcba" ?STR
-}T
-
