134701558fac17f6237c03a2f704043206e55f2e
Christophe Thibaut <cthibauttof@gmail.com>
2025-04-19 10:18:08 +0200

    parser of complex loner situations

--- LONER/Forth/src/loner.fs
+++ LONER/Forth/src/loner.fs
@@ -1,21 +1,47 @@
 \ --------- loner.fs --------
 
 S" 0" STR-P OPT-P CONSTANT 0*
+S" 0" STR-P OPT-P EOS-P SEQ-P CONSTANT 0*.
 S" 1" STR-P CONSTANT _1
 S" 110" STR-P CONSTANT _110
+S" 00" STR-P CONSTANT _00
+S" 01" STR-P CONSTANT _01
+S" 11" STR-P CONSTANT _11
+S" 01" STR-P REP-P CONSTANT _01+
+S" 10" STR-P REP-P CONSTANT _10+
+S" 11" STR-P REP-P CONSTANT _11+
 
-: <&> SEQ-P ;
+: & SEQ-P ;
 
-: <|> ALT-P ;
+: | ALT-P ;
 
-: :. EOS-P <&> ;
+0* _1   & 0*.  &                    CONSTANT LONER-A
+0* _110 & 0*.  &                    CONSTANT LONER-B
+0* _11+ & _01  & 0*.  &             CONSTANT LONER-C1
+0* _11  & _01+ & 0*.  &             CONSTANT LONER-C2
+0* _11  & _01+ & _11+ & _01 & 0*. & CONSTANT LONER-C3
+0* _11  & _00  & _10+ & _11 & 0*. & CONSTANT LONER-D1
+0* _11  & _00  & _11+ & 0*.       & CONSTANT LONER-D2
+0* _11  & _00  & _11+ & _10+ & _11 & 0*. & CONSTANT LONER-D3
+0* _11  & _01+ & _00  & _11  & 0*. & CONSTANT LONER-D4
+0* _11  & _01+ & _00  & _10+ & _11 & 0*. & CONSTANT LONER-D5
+0* _11  & _01+ & _00  & _11+ & 0*. & CONSTANT LONER-D6
+0* _11  & _01+ & _00  & _11+ & _10+ & _11 & 0*. & CONSTANT LONER-D7
 
-0* _1    <&> 0* <&> :. CONSTANT LONER-A
-0* _110  <&> 0* <&> :. CONSTANT LONER-B
-
-LONER-A LONER-B <|> CONSTANT LONER
+LONER-A
+LONER-B  |
+LONER-C1 |
+LONER-C2 |
+LONER-C3 |
+LONER-D1 |
+LONER-D2 |
+LONER-D3 |
+LONER-D4 |
+LONER-D5 |
+LONER-D6 |
+LONER-D7 |
+CONSTANT LONER
 
 
 : LONER? ( str,sc -- f )
     LONER EXECUTE -ROT 2DROP ;
-    
