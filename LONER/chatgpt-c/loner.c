4e7235de80dc608e36a8dfc08ab808002296ac12
Christophe Thibaut <cthibauttof@gmail.com>
2025-05-01 11:42:50 +0200

    pass 1

--- LONER/chatgpt-c/loner.c
+++ LONER/chatgpt-c/loner.c
@@ -2,28 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
-int can_win(char *board, int n) {
-    int count = 0;
-    for (int i = 0; i < n; i++)
-        if (board[i] == '1') count++;
-    if (count == 1) return 1;
+#define MAX_N 32000
 
-    for (int i = 0; i < n - 2; i++) {
+int can_win(char *board, int n, int remaining) {
+    if (remaining == 1) return 1;
+
+    for (int i = 0; i < n; i++) {
         // Move right
-        if (board[i] == '1' && board[i+1] == '1' && board[i+2] == '0') {
-            board[i] = board[i+1] = '0';
-            board[i+2] = '1';
-            if (can_win(board, n)) return 1;
-            board[i] = board[i+1] = '1';
-            board[i+2] = '0';
+        if (i + 2 < n && board[i] == '1' && board[i + 1] == '1' && board[i + 2] == '0') {
+            board[i] = board[i + 1] = '0';
+            board[i + 2] = '1';
+            if (can_win(board, n, remaining - 1)) return 1;
+            board[i] = board[i + 1] = '1';
+            board[i + 2] = '0';
         }
         // Move left
-        if (board[i] == '0' && board[i+1] == '1' && board[i+2] == '1') {
-            board[i] = '1';
-            board[i+1] = board[i+2] = '0';
-            if (can_win(board, n)) return 1;
-            board[i] = '0';
-            board[i+1] = board[i+2] = '1';
+        if (i - 2 >= 0 && board[i] == '1' && board[i - 1] == '1' && board[i - 2] == '0') {
+            board[i] = board[i - 1] = '0';
+            board[i - 2] = '1';
+            if (can_win(board, n, remaining - 1)) return 1;
+            board[i] = board[i - 1] = '1';
+            board[i - 2] = '0';
         }
     }
     return 0;
@@ -32,15 +31,24 @@ int can_win(char *board, int n) {
 int main() {
     int t;
     scanf("%d", &t);
+
     while (t--) {
         int n;
+        char board[MAX_N + 1];
         scanf("%d", &n);
-        char *board = malloc((n + 1) * sizeof(char));
         scanf("%s", board);
-        if (can_win(board, n)) printf("yes\n");
-        else printf("no\n");
-        free(board);
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (board[i] == '1') count++;
+        }
+
+        if (can_win(board, n, count))
+            printf("yes\n");
+        else
+            printf("no\n");
     }
+
     return 0;
 }
 
