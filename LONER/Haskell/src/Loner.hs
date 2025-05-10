2313893906ed597e892ea7a102be0fba586abc11
Christophe Thibaut <cthibauttof@gmail.com>
2025-05-10 18:34:12 +0200

    LONER Haskell parsing a,b,c

--- LONER/Haskell/src/Loner.hs
+++ LONER/Haskell/src/Loner.hs
@@ -1,69 +1,68 @@
-module Loner where
+module Loner (loner)
+    where
 
-import Data.Maybe
+type Parser = String -> (Bool, String)
 
-capture :: String -> Maybe String
-capture "110" = Just "001"
-capture "011" = Just "100"
-capture s = Nothing
+parseChar :: Char -> Parser
+parseChar c (x:xs) | c == x = (True, xs)
+parseChar _ xs     = (False,xs)
+
+eos :: Parser
+eos "" = (True,"")
+eos s  = (False, s)
+
+(<&>) :: Parser -> Parser -> Parser
+(parserA <&> parserB) s =
+        case parserA s of
+            (True, t) -> parserB t
+            r -> r
 
 
+(<|>) :: Parser -> Parser -> Parser
+(parserA <|> parserB) s =
+        case parserA s of
+            (False,_) -> parserB s
+            r -> r
 
-captures' :: String -> [Maybe String]
-captures' (a:b:c:xs) = fmap (++ xs) (capture [a,b,c]) : fmap (fmap ( a:)) (captures' (b:c:xs))
-captures' _ = []
+many :: Parser -> Parser
+many parser s = case parser s of
+                  (False, t) -> (True, t)
+                  (True, t) -> many parser t
 
-viable :: Maybe String -> Bool
-viable Nothing = False
-viable (Just s) = null $ filter (> 2) $ zipWith (-) (tail ps) ps
-    where ps = map fst $ filter (\(_,x) -> x == '1') $ zip [0..] s
+some :: Parser -> Parser
+some parser s = case parser s of
+                  (True, t) -> many parser t
+                  r -> r
+p :: Parser
+p = parseChar '1'
 
-moves' :: String -> [Maybe String]
-moves' (a:b:c:xs) = filter viable $ fmap (++ xs) (capture [a,b,c]) : fmap (fmap (a:)) (moves' (b:c:xs))
-moves' _ = []
+e :: Parser
+e = parseChar '0'
 
-moves :: String -> [String]
-moves = catMaybes . moves'
+a :: Parser
+a = many e <&> p <&> many e <&> eos
 
-evaluate :: [String] -> Bool
-evaluate [] = False
-evaluate bs | or $ map containsSinglePawn bs = True
-evaluate bs = if ms == bs then True else evaluate ms
-    where ms = bs >>= moves
+b :: Parser
+b = many e <&> p <&> p <&> e <&> many e <&> eos
 
-captures :: String -> [String]
-captures = catMaybes . captures'
+ep :: Parser
+ep = e <&> p
 
-containsSinglePawn :: String -> Bool
-containsSinglePawn ['1'] = True
-containsSinglePawn ['0'] = False
-containsSinglePawn ('0':xs) = containsSinglePawn xs
-containsSinglePawn ('1':xs) = not ('1' `elem` xs)
+pp :: Parser
+pp = p <&> p
 
-transform :: String -> String
-transform [] = []
-transform ('0':'1':'1':xs) = '1':'0':'0':transform xs
-transform ('1':'1':'0':xs) = '0':'0':'1':transform xs
-transform ('0':xs) = '0':transform xs
-transform ('1':xs) = '1':transform xs
+c0 :: Parser
+c0 = many e <&> pp <&> ep  <&> many e <&> eos
+
+c1 :: Parser
+c1 = many e <&> pp <&> some ep <&> many e <&> eos
+
+c2 :: Parser
+c2 = many e <&> some pp <&> ep <&> many e <&> eos
+
+c3 :: Parser
+c3 = many e <&> pp <&> some ep <&> some pp <&> ep <&> many e <&> eos
 
 loner :: String -> Bool
-loner s | containsSinglePawn (transform s) = True
-loner s = False
-
-readInt :: IO Int
-readInt = readLn 
-
-processTestCase :: Int -> IO ()
-processTestCase 0 = return ()
-processTestCase n = do
-    l <- readInt
-    s <- getLine
-    putStrLn $ if evaluate [take l s] then "yes" else "no"
-    processTestCase (pred n)
-
-process :: IO ()
-process = do
-    t <- readInt
-    processTestCase t
+loner = fst . (a <|> b <|> c0 <|> c1 <|> c2 <|> c3)
 
