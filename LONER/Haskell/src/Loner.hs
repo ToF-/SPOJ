e314ec2523f246ed6dd5fbdb4211f4b095f93d5e
Christophe Thibaut <cthibauttof@gmail.com>
2025-05-11 11:44:40 +0200

    all patterns loner haskell

--- LONER/Haskell/src/Loner.hs
+++ LONER/Haskell/src/Loner.hs
@@ -29,7 +29,7 @@ infixl 6 <|>
 
 many :: Parser -> Parser
 many parser s = case parser s of
-                  (False, t) -> (True, s)
+                  (False, _) -> (True, s)
                   (True, t) -> many parser t
 
 some :: Parser -> Parser
@@ -42,11 +42,12 @@ p = parseChar '1'
 e :: Parser
 e = parseChar '0'
 
-a :: Parser
-a = many e <&> p <&> many e <&> eos
 
-b :: Parser
-b = many e <&> pp <&> e <&> many e <&> eos
+a_patterns :: Parser
+a_patterns = es <&> p <&> es <&> eos
+
+b_patterns :: Parser
+b_patterns = es <&> pp <&> e <&> es <&> eos
 
 ep :: Parser
 ep = e <&> p
@@ -60,45 +61,78 @@ pp = p <&> p
 ee :: Parser
 ee = e <&> e
 
-c :: Parser
-c = c1 <|> c2 <|> c3
+es :: Parser
+es = many e
+
+c_patterns :: Parser
+c_patterns = c1 <|> c2 <|> c3
 
 c1 :: Parser
-c1 = many e <&> pp <&> some ep <&> many e <&> eos
+c1 = es <&> pp <&> some ep <&> es <&> eos
 
 c2 :: Parser
-c2 = many e <&> some pp <&> ep <&> many e <&> eos
+c2 = es <&> some pp <&> ep <&> es <&> eos
 
 c3 :: Parser
-c3 = many e <&> pp <&> some ep <&> some pp <&> ep <&> many e <&> eos
+c3 = es <&> pp <&> some ep <&> some pp <&> ep <&> es <&> eos
 
-d :: Parser
-d = d0 <|> d1 <|> d2 <|> d3 <|> d4 <|> d5 <|> d6 <|> d7
+d_patterns :: Parser
+d_patterns = d0 <|> d1 <|> d2 <|> d3 <|> d4 <|> d5 <|> d6 <|> d7
 
 d0 :: Parser
-d0 = many e <&> pp <&> ee <&> pp <&> many e <&> eos
+d0 = es <&> pp <&> ee <&> pp <&> es <&> eos
 
 d1 :: Parser
-d1 = many e <&> pp <&> some ep <&> ee <&> pp <&> many e <&> eos
+d1 = es <&> pp <&> some ep <&> ee <&> pp <&> es <&> eos
 
 d2 :: Parser
-d2 = many e <&> pp <&> ee <&> some pp <&> many e <&> eos
+d2 = es <&> pp <&> ee <&> some pp <&> es <&> eos
 
 d3 :: Parser
-d3 = many e <&> pp <&> some ep <&> ee <&> some pp <&> many e <&> eos
+d3 = es <&> pp <&> some ep <&> ee <&> some pp <&> es <&> eos
 
 d4 :: Parser
-d4 = many e <&> pp <&> ee <&> some pe <&> pp <&> many e <&> eos
+d4 = es <&> pp <&> ee <&> some pe <&> pp <&> es <&> eos
 
 d5 :: Parser
-d5 = many e <&> pp <&> some ep <&> ee <&> some pe <&> pp <&> many e <&> eos
+d5 = es <&> pp <&> some ep <&> ee <&> some pe <&> pp <&> es <&> eos
 
 d6 :: Parser
-d6 = many e <&> pp <&> ee <&> some pp <&> some pe <&> pp <&> many e <&> eos
+d6 = es <&> pp <&> ee <&> some pp <&> some pe <&> pp <&> es <&> eos
 
 d7 :: Parser
-d7 = many e <&> pp <&> some ep <&> ee <&> some pp <&> some pe <&> pp <&> many e <&> eos
+d7 = es <&> pp <&> some ep <&> ee <&> some pp <&> some pe <&> pp <&> es <&> eos
+
+e_patterns :: Parser
+e_patterns = e0 <|> e1 <|> e2 <|> e3 <|> e4 <|> e5 <|> e6 <|> e7
+
+e0 :: Parser
+e0 = es <&> pp <&> pp <&> ep <&> pp <&> es <&> eos
+
+e1 :: Parser
+e1 = es <&> pp <&> some ep <&> pp <&> ep <&> pp <&> es <&> eos
+
+e2 :: Parser
+e2 = es <&> pp <&> pp <&> ep <&> some pp <&> es <&> eos
+
+e3 :: Parser
+e3 = es <&> pp <&> some ep <&> pp <&> ep <&> some pp <&> es <&> eos
+
+e4 :: Parser
+e4 = es <&> pp <&> pp <&> ep <&> some pe <&> pp <&> es <&> eos
+
+e5 :: Parser
+e5 = es <&> pp <&> some ep <&> pp <&> ep <&> some pe <&> pp <&> es <&> eos
+
+e6 :: Parser
+e6 = es <&> pp <&> pp <&> ep <&> some pp <&> some pe <&> pp <&> es <&> eos
+
+e7 :: Parser
+e7 = es <&> pp <&> some ep <&> pp <&> ep <&> some pp <&> some pe <&> pp <&> es <&> eos
+
+all_patterns :: Parser
+all_patterns = a_patterns <|> b_patterns <|> c_patterns <|> d_patterns <|> e_patterns
 
 loner :: String -> Bool
-loner = fst . ( a <|> b <|> c <|> d )
+loner s = fst (all_patterns s) || fst (all_patterns (reverse s))
 
--- LONER/Haskell/test/LonerSpec.hs
+++ LONER/Haskell/test/LonerSpec.hs
@@ -4,37 +4,69 @@ module LonerSpec (spec)
 import Test.Hspec
 import Loner
 
+checkLoner :: String -> Expectation
+checkLoner s = do
+    case loner s of
+        True -> return ()
+        False -> putStrLn $ "loner \"" ++ s ++ "\" `shouldBe` True"
+    loner s `shouldBe` True
+
+checkNotLoner :: String -> Expectation
+checkNotLoner s = do
+    case loner s of
+        False -> return ()
+        True -> putStrLn $ "loner \"" ++ s ++ "\" `shouldBe` False"
+    loner s `shouldBe` False
+
 spec :: SpecWith ()
 spec = do
     describe "loner" $ do
         it "evaluate trivial cases" $ do
-            loner "1" `shouldBe` True
-            loner "0" `shouldBe` False
-            loner ""  `shouldBe` False
+            checkLoner "1"
+            checkNotLoner "0"
+            checkNotLoner ""
 
         it "evaluate simple cases" $ do
-            loner "10" `shouldBe` True
-            loner "11" `shouldBe` False
-            loner "100" `shouldBe` True
-            loner "101" `shouldBe` False
-            loner "001" `shouldBe` True
+            checkLoner "10"
+            checkNotLoner "11"
+            checkLoner "100"
+            checkNotLoner "101"
+            checkLoner "001"
 
         it "evaluate 3 sized boards" $ do
-            loner "110" `shouldBe` True
-            loner "111" `shouldBe` False 
-            loner "0000110000" `shouldBe` True
+            checkLoner "110"
+            checkNotLoner "111"
+            checkLoner "0000110000"
 
         it "evaluate complex C boards" $ do
-            loner "1101" `shouldBe` True 
-            loner "110101" `shouldBe` True
-            loner "111101" `shouldBe` True 
-            loner "110101111101" `shouldBe` True
+            checkLoner "1101" 
+            checkLoner "110101"
+            checkLoner "111101" 
+            checkLoner "110101111101"
 
         it "evaluate complex D boards" $ do
-            loner "110011" `shouldBe` True 
-            loner "1100111111" `shouldBe` True
-            loner "110101001111" `shouldBe` True
-            loner "110010101011" `shouldBe` True 
-            loner "11010100101011" `shouldBe` True 
-            loner "11001111101011" `shouldBe` True
-            loner "110101001111101011" `shouldBe` True
+            checkLoner "110011" 
+            checkLoner "1101010011"
+            checkLoner "1100111111"
+            checkLoner "110101001111"
+            checkLoner "110010101011" 
+            checkLoner "11010100101011" 
+            checkLoner "11001111101011"
+            checkLoner "110101001111101011"
+
+        it "evaluate complex E boards" $ do
+            checkLoner "111101110"
+            checkLoner "110101110111"
+            checkLoner "1111011111"
+            checkLoner "11010111011111"
+            checkLoner "111101101011"
+            checkLoner "1101011101101011"
+            checkLoner "1111011111101011"
+            checkLoner "11010111011111101011"
+
+        it "evaluate boards with trailings" $ do
+            checkLoner "00000000011010111011111101011000000000000000"
+
+        it "evaluate boards in both directions" $ do
+            let board = reverse "11010111011111101011"
+            checkLoner board
