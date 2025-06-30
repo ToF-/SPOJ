71f11be6422fb97b896d890c2560a568ae2ff0a2
Christophe Thibaut <cthibauttof@gmail.com>
2025-06-30 23:49:40 +0200

    max lower anagram wip

--- ANADIV/CLisp/src/anadiv.lisp
+++ ANADIV/CLisp/src/anadiv.lisp
@@ -2,66 +2,36 @@
   (with-input-from-string (s input)
     (cons (read s) (read s))))
 
-(defun nal (n)
-  (if (= n 0)
-    ()
-    (multiple-value-bind (q r)
-      (truncate n 10)
-      (cons r (nal q)))))
+(defun digits (n)
 
-(defun lan (elements)
-  (if (null elements)
-    0
-    (+ (car elements) (* 10 (lan (cdr elements))))))
+  (defun digits-aux (nb)
+    (if (= nb 0)
+      ()
+      (multiple-value-bind (qu re)
+        (truncate nb 10)
+        (cons re (digits-aux qu)))))
 
-(defun split-digits (digits)
-  (defun split-digits-aux (dgts last-digit left)
-    (if (null dgts)
-      (list left '())
-      (let ((digit (car dgts)))
-        (if (< digit last-digit)
-          (list left dgts)
-          (split-digits-aux (cdr dgts) digit (append left (list digit)))))))
-  (split-digits-aux digits 0 '()))
+  (sort (digits-aux n) #'<))
+
+
+(defun split-digits (dgts)
+
+  (defun split-digits-aux (digits result)
+    (cond ((null digits)
+           (list (reverse result)))
+          ((or
+             (null (cdr digits))
+             (<= (car digits) (cadr digits)))
+           (split-digits-aux (cdr digits) (cons (car digits) result)))
+          (t (cons
+               (reverse (cons (car digits) result))
+               (cdr digits)))))
+
+  (split-digits-aux dgts '()))
 
 (defun max-lower-anagram (digits)
   (let ((split (split-digits digits)))
-    (if (null (cadr split))
-      '()
-      (let* ((pref (car split))
-             (suff (cadr split))
-             (l (car (last pref)))
-             (pre (butlast pref))
-             (sub (sort suff #'>))
-             (new (car sub))
-             (post (cdr sub)))
-        (format t "pref:~A suff:~A pre:~A l:~A sub:~A new:~A post:~A~%" pref suff pre l sub new post)
-        (append pre (list new) (sort (cons l post) #'> ))))))
-
-
-(defun nal-9-complement (digits)
-  (if (null digits)
-    ()
-    (cons (- 9 (car digits)) (nal-9-complement (cdr digits)))))
-
-(defun nal-minus (a b)
   (cond
-    ((null b) a)
-    (t (cons (- (car a) (car b)) (nal-minus (cdr a) (cdr b))))))
-
-(defun digits (n)
-  (sort (nal n) #'<))
-
-(defun all-permutations (elements)
-  (cond
-    ((null elements) (list elements))
-    ((null (cdr elements)) (list elements))
-    (t (loop for element in elements
-             append
-             (mapcar
-               (lambda (sublist) (cons element sublist))
-               (all-permutations
-                 (remove element elements)))))))
-
-
+    ((null (cdr split)) ())
+    (t (append (cdr split) (list (caar split)))))))
 
--- ANADIV/CLisp/test/anadiv-test.lisp
+++ ANADIV/CLisp/test/anadiv-test.lisp
@@ -14,51 +14,24 @@
              )
 
 (define-test split-digits
-             (assert-equal '((1) ()) (split-digits '(1)))
-             (assert-equal '((4 8) (0 7)) (split-digits '(4 8 0 7)))
-             (assert-equal '((3) (2 7 6 8)) (split-digits '(3 2 7 6 8)))
-             (assert-equal '((9) (8 6 5 1)) (split-digits '(9 8 6 5 1)))
+             (assert-equal '((1)) (split-digits '(1)))
+             (assert-equal '((1 3)) (split-digits '(1 3)))
+             (assert-equal '((0 4 7 8)) (split-digits '(0 4 7 8)))
+             (assert-equal '((7) 2) (split-digits '(7 2)))
+             (assert-equal '((7 9) 2 5 3) (split-digits '(7 9 2 5 3)))
+             (assert-equal '((4 8) 0 7) (split-digits '(4 8 0 7)))
+             (assert-equal '((3) 2 7 6 8) (split-digits '(3 2 7 6 8)))
+             (assert-equal '((9) 8 6 5 1) (split-digits '(9 8 6 5 1)))
              )
 
 (define-test max-lower-anagram
-             (assert-equal '() (max-lower-anagram '(0 4 7 8)))
-             (assert-equal '(0 4 7 8) (max-lower-anagram '(0 4 8 7)))
-             (assert-equal '(4 7 8 0) (max-lower-anagram '(4 8 0 7)))
-             (assert-equal '(4 7 0 8) (max-lower-anagram '(4 7 8 0)))
-             (assert-equal '(4 0 8 7) (max-lower-anagram '(4 7 0 8)))
+             (assert-equal '() (max-lower-anagram '(1)))
+             (assert-equal '() (max-lower-anagram '(1 2 5 7 8 9)))
+             (assert-equal '(1 2) (max-lower-anagram '(2 1)))
+             (assert-equal '(2 4) (max-lower-anagram '(4 2)))
+             (assert-equal '(2 5 4) (max-lower-anagram '(4 2 5)))
+             (assert-equal '(4 1 5 2) (max-lower-anagram '(4 2 1 5)))
              )
-
-(define-test number-as-list
-             (assert-equal '(7 0 8 4) (nal 4807))
-             (assert-equal '(1 8 2) (nal 281))
-             )
-
-(define-test list-as-number
-             (assert-equal 7 (lan '(7)))
-             (assert-equal 217 (lan '(7 1 2)))
-             (assert-equal 7084 (lan '(4 8 0 7)))
-             (let ((n (random 100000)))
-                 (assert-equal n (lan (nal n))))
-             )
-
-(define-test number-as-list-9-complement
-             (assert-equal 8 (lan (nal-9-complement (nal 1))))
-             (assert-equal 5192 (lan (nal-9-complement (nal 4807))))
-             )
-
-(define-test number-as-list-minus
-             (assert-equal 4 (lan (nal-minus (nal 7) (nal 3))))
-             (assert-equal 15 (lan (nal-minus (nal 18) (nal 3))))
-             (assert-equal 17 (lan (nal-minus (nal 22) (nal 5))))
-             )
-
-(define-test all-permutations
-             (assert-equal '(()) (all-permutations '()))
-             (assert-equal '((1)) (all-permutations '(1)))
-             (assert-equal '((1 2) (2 1)) (all-permutations '(1 2)))
-             (assert-equal '((1 2 3) (1 3 2) (2 1 3) (2 3 1) (3 1 2) (3 2 1)) (all-permutations '(1 2 3)))
-             )
-
 
 (run-tests :all)
 (sb-ext:quit)
