29887c99a13dc500fe02a7f84b72ac017fe18aec
Christophe Thibaut <cthibauttof@gmail.com>
2025-07-05 21:47:38 +0200

    divisibles

--- ANADIV/CLisp/src/anadiv.lisp
+++ ANADIV/CLisp/src/anadiv.lisp
@@ -41,7 +41,9 @@
 
 (defun multiple (operand m)
   (if (= (length operand) 1)
-    (or (= m (car operand)) (= 0 (car operand)))
+    (if (>= (car operand) m)
+      (multiple (list (- (car operand) m)) m)
+      (= (car operand) 0))
     (multiple (subtract operand (list m)) m)))
 
 (defun add (operand addend)
@@ -63,7 +65,9 @@
         (cons (- (+ o a) 10) (add (increment os) as))))))
 
 (defun sum-digits (operand)
-  (digits-from-number (apply #'+ operand)))
+  (if (null operand)
+    (list 0)
+    (add (sum-digits (cdr operand)) (list (car operand)))))
 
 (defun divisible-by-7 (digits)
   (if (< (length digits) 3)
@@ -71,176 +75,31 @@
     (let ((subtrahend (digits-from-number (* 2 (car digits)))))
       (divisible-by-7 (subtract (cdr digits) subtrahend)))))
 
+(defun divisible-by-8 (digits)
+  (if (< (length digits) 3)
+    (multiple digits 8)
+    (multiple (list (car digits)
+                    (cadr digits)
+                    (caddr digits)) 8)))
 
-; (defun digits-to-number (digits)
-; 
-;   (defun digits-to-number-aux (dgts result)
-;     (if (null dgts)
-;       result
-;       (digits-to-number-aux (cdr dgts) (+ (* result 10) (car dgts)))))
-; 
-;   (digits-to-number-aux digits 0))
-; 
-; (defun number-to-digits (n)
-; 
-;   (defun number-to-digits-aux (nb result)
-;     (if (= nb 0)
-;       result
-;       (multiple-value-bind (qu re)
-;         (truncate nb 10)
-;         (number-to-digits-aux qu (cons re result)))))
-; 
-;   (number-to-digits-aux n ()))
-; 
-; (defun digits (n)
-;   (sort (number-to-digits n) #'<))
-; 
-; (defun split-digits (dgts)
-; 
-;   (defun split-digits-aux (digits result)
-;     (cond ((null digits)
-;            (list (reverse result)))
-;           ((or
-;              (null (cdr digits))
-;              (<= (car digits) (cadr digits)))
-;            (split-digits-aux (cdr digits) (cons (car digits) result)))
-;           (t (cons
-;                (reverse (cons (car digits) result))
-;                (cdr digits)))))
-; 
-;   (split-digits-aux dgts '()))
-; 
-; (defun max-anagram (digits)
-;   (sort digits #'>))
-; 
-; (defun extract-lower (digit dgts)
-;   
-;   (defun extract-lower-aux (digits result)
-;     (cond
-;       ((null digits) result)
-;       ((< (car digits) digit) (cons (car digits) (append (cdr digits) result)))
-;       (t (extract-lower-aux (cdr digits) (cons (car digits) result)))))
-; 
-;   (extract-lower-aux (sort dgts #'>) ()))
-; 
-; (defun max-lower-anagram (digits)
-;   (let ((split (split-digits digits)))
-;   (if (null (cdr split))
-;     ()
-;     (let ((suff (max-lower-anagram (cdr split))))
-;       (if suff
-;         (append (car split) suff)
-;         (let* ((prefix (reverse (car split)))
-;                (suffix (cdr split))
-;                (split-digit (car prefix))
-;                (split-rem   (cdr prefix))
-;                (extract (extract-lower split-digit suffix))
-;                (new-digit (car extract))
-;                (new-suffix (cdr extract)))
-;           (append (reverse (cons new-digit split-rem))
-;                   (sort (cons split-digit new-suffix) #'>))))))))
-; 
-; (defun max-lower-anagrams (digits)
-;   (if (null digits)
-;     ()
-;     (progn
-;       (format t "~A~%" (digits-to-number digits))
-;       (max-lower-anagrams (max-lower-anagram digits)))))
-; 
-; (defun digit-subtract (digits sub)
-;   
-;   (defun decrease (digits)
-;     (cond
-;       ((null digits) ())
-;       ((= 0 (car digits)) (cons 9 (decrease (cdr digits))))
-;       (t (cons (- (car digits) 1) (cdr digits)))))
-; 
-;   (defun digit-subtract-aux (digits sub result)
-;     (format t "digits:~A sub:~A result:~A~%" digits sub result)
-;     (cond
-;       ((null digits) result)
-;       ((null sub) (append result digits))
-;       ((<= (car sub) (car digits))
-;        (digit-subtract-aux (cdr digits) (cdr sub)
-;                            (append result (list (- (car digits) (car sub))))))
-;       (t
-;         (digit-subtract-aux (decrease (cdr digits)) (cdr sub)
-;                             (append result (list (- (+ 10 (car digits)) (car sub))))))))
-; 
-;   (reverse (digit-subtract-aux (reverse digits) (reverse sub) ())))
-; 
-; (defun divisible-8 (digits)
-; 
-;   (defun first-three (dgts)
-;     (cons (car dgts)
-;           (cons (cadr dgts)
-;           (list (caddr dgts)))))
-; 
-;   (let ((m (apply #'+ (if (> (length digits) 3)
-;                         (first-three (reverse digits)) digits))))
-;     (= 0 (rem m 8))))
-; 
-; (defun divisible (digits n)
-;   (let ((stigid (reverse digits)))
-;     (cond
-;       ((= 1 n) t)
-;       ((= 2 n) (evenp (car stigid)))
-;       ((= 3 n) (= 0 (rem (apply #'+ digits) 3)))
-;       ((= 4 n)
-;        (if (>= (length digits) 2)
-;          (= 0 (rem (+ (car stigid) (cadr stigid)) 4))
-;          (= 0 (rem (car stigid) 4))))
-;       ((= 5 n)
-;        (or (= 0 (car stigid)) (= 5 (car stigid))))
-;       ((= 6 n)
-;        (and (= 0 (rem (apply #'+ digits) 3)) (evenp (car stigid))))
-;       ((= 7 n)
-;        (divisible-7 digits))
-;       ((= 8 n)
-;        (divisible-8 digits))
-;       ((= 9 n)
-;        (= 0 (rem (apply #'+ digits) 9)))
-;       ((= 10 n)
-;        (= 0 (car (reverse digits))))
-;       )))
-; 
-; (defun sum-divisible-aux (digits k acc)
-;   (cond
-;     ((>= acc k) (sum-divisible-aux digits k (- acc k)))
-;     ((null digits) (= acc 0))
-;     (t (sum-divisible-aux (cdr digits) k (+ acc (car digits))))))
-; 
-; (defun sum-divisible (dgts k)
-;   (sum-divisible-aux dgts k 1))
-;       
-; (defun divisible-7 (digits)
-;   (if (< (length digits) 4)
-;     (
-;     (= 0 (rem (digits-to-number digits) 7))
-;     (let* ((digit (car (last digits)))
-;            (remain (butlast digits))
-;            (sub (number-to-digits (* digit 2))))
-;       (divisible-7 (digit-subtract remain sub))))))
-; 
-; (defun is-divisible (digits k)
-;   (let ((predicates
-;           (list
-;             #'(lambda (x) nil)
-;             #'(lambda (x) t)
-;             #'(lambda (x) (evenp (car (reverse x))))
-;             #'(lambda (x) (sum-divisible x 3))
-;             #'(lambda (x)
-;                 (sum-divisible (if (> (length x) 1)
-;                                  (list (+ (* (cadr x) 10) (car x)))
-;                                  (list (car x))) 4))
-;             #'(lambda (x) (or (= 5 (car x)) (= 0 (car x))))
-;             #'(lambda (x) (and (sum-divisible x 3) (evenp (car (reverse x)))))
-;             #'(lambda (x) (divisible-7 x))
-;             #'(lambda (x) (sum-divisible
-;                             (let ((xxx (append x (list 0 0))))
-;                               (+ (* 100 (caddr xxx)) (* 10 (cadr xxx)) (car xxx))) 8))
-;             #'(lambda(x) (sum-divisible x 9))
-;             #'(lambda(x) (= 0 (car x)))
-;             )))
-;     (funcall (nth k predicates) (reverse digits))))
-; 
+(defun divisible-by-9 (digits)
+  (multiple (sum-digits digits) 9))
+
+(defun divisible-by-3 (digits)
+  (multiple (sum-digits digits) 3))
+
+(defun divisible-by-4 (digits)
+  (if (< (length digits) 3)
+    (multiple digits 4)
+    (multiple (list
+                (car digits)
+                (cadr digits)) 4)))
+
+(defun divisible-by-5 (digits)
+  (or (= (car digits) 0) (= (car digits) 5)))
+
+(defun divisible-by-2 (digits)
+  (evenp (car digits)))
+
+(defun divisible-by-10 (digits)
+  (= (car digits) 0))
--- ANADIV/CLisp/test/anadiv-test.lisp
+++ ANADIV/CLisp/test/anadiv-test.lisp
@@ -62,6 +62,7 @@
                (assert-equal result (value (sum-digits (digits-from-number operand)))))
 
              (assert-sum-digits 19 4807)
+             (assert-sum-digits 22 (* 7 7 7 7 7))
              )
 
 (define-test divisible-by-7
@@ -69,83 +70,48 @@
              (assert-equal nil (divisible-by-7 (digits-from-number 6)))
              (assert-equal t (divisible-by-7 (digits-from-number (* 7 7 7 7 7 7 7 7 7 7 7))))
              )
-; 
-; 
-; (define-test digits
-;              (assert-equal '(0 4 7 8) (digits 4807))
-;              (assert-equal '(2 4 5 9 9) (digits 54929))
-;              )
-; 
-; (define-test digits-to-number
-;              (assert-equal 4807 (digits-to-number '(4 8 0 7)))
-;              )
-; 
-; (define-test number-to-digits
-;              (assert-equal '(4 8 0 7) (number-to-digits 4807))
-;              )
-; 
-; (define-test max-anagram
-;              (assert-equal '(8 7 4 0) (max-anagram '(4 8 0 7)))
-;              )
-; (define-test split-digits
-;              (assert-equal '((1)) (split-digits '(1)))
-;              (assert-equal '((1 3)) (split-digits '(1 3)))
-;              (assert-equal '((0 4 7 8)) (split-digits '(0 4 7 8)))
-;              (assert-equal '((7) 2) (split-digits '(7 2)))
-;              (assert-equal '((7 9) 2 5 3) (split-digits '(7 9 2 5 3)))
-;              (assert-equal '((4 8) 0 7) (split-digits '(4 8 0 7)))
-;              (assert-equal '((3) 2 7 6 8) (split-digits '(3 2 7 6 8)))
-;              (assert-equal '((9) 8 6 5 1) (split-digits '(9 8 6 5 1)))
-;              )
-; 
-; (define-test extract-lower
-;              (assert-equal '(4 0 7 8) (extract-lower 7 '(4 8 0 7)))
-;              (assert-equal '(7 4 0 8) (extract-lower 8 '(4 8 0 7)))
-;              (assert-equal '(0 4 7 8) (extract-lower 4 '(4 8 0 7)))
-;              (assert-equal '(8 7 4 0) (extract-lower 9 '(4 8 0 7)))
-;              )
-; 
-; (define-test max-lower-anagram
-;              (assert-equal '() (max-lower-anagram '(1)))
-;              (assert-equal '() (max-lower-anagram '(1 2 5 7 8 9)))
-;              (assert-equal '(1 4 2 3) (max-lower-anagram '(1 4 3 2)))
-;              (assert-equal '(1 2) (max-lower-anagram '(2 1)))
-;              (assert-equal '(2 4) (max-lower-anagram '(4 2)))
-;              (assert-equal '(1 3 5 4 2) (max-lower-anagram '(1 4 2 3 5)))
-;              (assert-equal '(3 5 4 2) (max-lower-anagram '(4 2 3 5)))
-;              (assert-equal '() (max-lower-anagram '(2 3 5)))
-;              (assert-equal '(4 1 5 2) (max-lower-anagram '(4 2 1 5)))
-;              )
-; 
-; (define-test digit-subtract
-;              (assert-equal '(5) (digit-subtract '(7) '(2)))
-;              (assert-equal '(1 5) (digit-subtract '(1 7) '(2)))
-;              (assert-equal '(1 2 3) (digit-subtract '(1 3 4) '(1 1)))
-;              (assert-equal '(1 2 3 4) (digit-subtract '(1 4 5 6) '(2 2 2)))
-;              (assert-equal '(2 3) (digit-subtract '(3 1) '(8)))
-;              (assert-equal '(0 9 8 9)    (digit-subtract '(1 0 0 6) '(1 7)))
-;              )
-; 
-; (define-test divisible
-;              (assert-equal t (divisible (number-to-digits 4807) 1))
-;              (assert-equal nil (divisible (number-to-digits 4807) 2))
-;              (assert-equal t (divisible (number-to-digits 32768) 2))
-;              (assert-equal nil (divisible (number-to-digits 4807) 3))
-;              (assert-equal t (divisible (number-to-digits (* 3 4807)) 3))
-;              (assert-equal nil (divisible (number-to-digits 4807) 4))
-;              (assert-equal t (divisible (number-to-digits 4848) 4))
-;              (assert-equal nil (divisible (number-to-digits 4807) 5))
-;              (assert-equal t (divisible (number-to-digits 4810) 5))
-;              (assert-equal nil (divisible (number-to-digits 4807) 6))
-;              (assert-equal t (divisible (number-to-digits 4806) 6))
-;              (assert-equal t (divisible (number-to-digits (* 14 14 4807)) 7))
-;              (assert-equal nil (divisible (number-to-digits 4807) 7))
-;              (assert-equal t (divisible (number-to-digits 4862) 8))
-;              (assert-equal nil (divisible (number-to-digits 4807) 8))
-;              (assert-equal nil (divisible (number-to-digits 4807) 9))
-;              (assert-equal t (divisible (number-to-digits 4806) 9))
-;              (assert-equal nil (divisible (number-to-digits 4807) 10))
-;              (assert-equal t (divisible (number-to-digits 4810) 10))
-;              )
+
+(define-test divisible-by-8
+             (assert-equal t (divisible-by-8 (digits-from-number 8)))
+             (assert-equal nil (divisible-by-8 (digits-from-number 7)))
+             (assert-equal t (divisible-by-8 (digits-from-number 256)))
+             (assert-equal t (divisible-by-8 (digits-from-number (* 8 8 8 8 8 8 8 8 8 8 8 8 8))))
+             (assert-equal nil (divisible-by-8 (digits-from-number (1+ (* 8 8 8 8 8 8 8 8 8 8 8)))))
+             )
+
+(define-test divisible-by-9
+             (assert-equal t (divisible-by-9 (digits-from-number (* 9 9 9 9 9 9 9 9 9 9 9))))
+             (assert-equal t (divisible-by-9 (digits-from-number (* 9 9 9 9 9 9 9 9 9 9 9))))
+             )
+
+(define-test divisible-by-3
+             (assert-equal t (divisible-by-3 (digits-from-number (* 3 3 3 3 3 3 3 3 3 3))))
+             (assert-equal nil (divisible-by-3 (digits-from-number (1+ (* 3 3 3 3 3 3 3 3 3 3)))))
+             )
+
+(define-test divisible-by-4
+             (assert-equal t (divisible-by-4 (digits-from-number 12)))
+             (assert-equal t (divisible-by-4 (digits-from-number (* 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4))))
+             (assert-equal nil (divisible-by-4 (digits-from-number (1+ (* 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4)))))
+             )
+
+(define-test divisible-by-5
+             (assert-equal t (divisible-by-5 (digits-from-number 120)))
+             (assert-equal t (divisible-by-5 (digits-from-number (* 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5))))
+             (assert-equal nil (divisible-by-5 (digits-from-number (1+ (* 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5)))))
+             )
+
+(define-test divisible-by-2
+             (assert-equal t (divisible-by-2 (digits-from-number 120)))
+             (assert-equal t (divisible-by-2 (digits-from-number (* 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2))))
+             (assert-equal nil (divisible-by-2 (digits-from-number (1+ (* 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2)))))
+             )
+(define-test divisible-by-10
+             (assert-equal t (divisible-by-10 (digits-from-number 120)))
+             (assert-equal t (divisible-by-10 (digits-from-number (* 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10))))
+             (assert-equal nil (divisible-by-10 (digits-from-number (1+ (* 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10)))))
+             )
+
+
 (run-tests :all)
 (sb-ext:quit)
