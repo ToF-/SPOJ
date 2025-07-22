d2fd14b0cf51f6e42d12d61d9780a997cb4e819b
Christophe Thibaut <cthibauttof@gmail.com>
2025-07-22 16:09:49 +0200

    oracle next-anagram debug with format

--- ANADIV/oracle/src/anadiv.lisp
+++ ANADIV/oracle/src/anadiv.lisp
@@ -1,23 +1,78 @@
 ; anadiv.lisp
 
-(defun max-subsequence (l n)
-  (append
-    (sort (subseq l 0 n) #'<)
-    (subseq l n)))
+(defun show (a)
+  (format t "← ~A~%" a)
+  a)
 
-(defun descending-prefix (l)
-  (defun descending-prefix-aux (l prefix)
+(defun digits (n)
+  (format t "(digits ~A)~%" n)
+  (defun digits-aux (n)
+    (if (= n 0)
+      ()
+      (multiple-value-bind (q r)
+        (truncate n 10)
+        (cons r (digits-aux q)))))
+  (show (if (= n 0)
+    '(0)
+    (digits-aux n))))
+
+(defun max-subseq (l n)
+  (format t "(max-subseq ~A ~A)~%" l n)
+  (show (append
+    (sort (subseq l 0 n) #'<)
+    (subseq l n))))
+
+(defun desc-prefix (l)
+  (format t "(desc-prefix ~A)~%" l)
+  (defun desc-prefix-aux (l prefix)
     (cond
       ((null l)
        (cons (reverse prefix) l))
       ((null prefix)
-       (descending-prefix-aux (cdr l) (cons (car l) prefix)))
+       (desc-prefix-aux (cdr l) (cons (car l) prefix)))
       ((<= (car l) (car prefix))
-       (descending-prefix-aux (cdr l) (cons (car l) prefix)))
+       (desc-prefix-aux (cdr l) (cons (car l) prefix)))
       (t
         (cons (reverse prefix) l))))
-  (descending-prefix-aux l ()))
+  (show (desc-prefix-aux l ())))
 
 
-(defun swap-digits-and-reorder (sp)
-  '(7 0 4 8))
+(defun to-swap (sp)
+  (format t "(to-swap ~A)~%" sp)
+  (defun find-pivot (prefix limit pivot result)
+    (format t "(find-pivot (~A ~A ~A ~A)~%" prefix limit pivot result)
+    (cond
+      ((null prefix) (cons pivot result))
+      ((> (car prefix) limit) (find-pivot (cdr prefix) limit pivot (cons (car prefix) result)))
+      ((> (car prefix) pivot) (find-pivot (cdr prefix) limit (car prefix) (cons pivot result)))
+      ((<= (car prefix) pivot) (find-pivot (cdr prefix) limit pivot (cons (car prefix) result)))))
+  (show (if (null (cdr sp))
+    nil
+    (let ((prefix (sort (car sp) #'<))
+          (limit (cadr sp)))
+      (cons
+        (find-pivot (cdr prefix) limit (car prefix) ())
+        (cdr sp))))))
+
+(defun swap (sp)
+  (format t "(swap ~A)~%" sp)
+  (show (if (null sp)
+    nil
+    (let ((prefix (car sp))
+          (suffix (cdr sp)))
+      (append (max-subseq (cons (car suffix) (cdr prefix)) (length prefix))
+              (cons (car prefix) (cdr suffix)))))))
+
+(defun number- (d)
+  (format t "(number- ~A)~%" d)
+  (if (null d)
+    0
+    (+ (car d) (* 10 (number- (cdr d))))))
+
+(defun max-anagram (n)
+  (let ((d (digits n)))
+    (number- (max-subseq d (length d)))))
+
+(defun next-anagram (n)
+  (number- (swap (to-swap (desc-prefix (digits n))))))
+
--- ANADIV/oracle/test/anadiv-test.lisp
+++ ANADIV/oracle/test/anadiv-test.lisp
@@ -7,28 +7,72 @@
 (load "src/anadiv")
 
 
-(define-test max-subsequence-on-full-length
-             (assert-equal '(0 4 7 8) (max-subsequence '(4 8 0 7) 4))
+(define-test digits
+              (assert-equal '(7 0 8 4) (digits 4807))
+              (assert-equal '(1) (digits 1))
+              (assert-equal '(0) (digits 0))
+              )
+
+(define-test max-subseq-on-full-length
+             (assert-equal '(0 4 7 8) (max-subseq '(4 8 0 7) 4))
              )
 
-(define-test max-subsequence-on-partial-length
-             (assert-equal '(4 8 0 7) (max-subsequence '(8 4 0 7) 2))
-             (assert-equal '(0 2 3 1 9) (max-subsequence '(2 0 3 1 9) 3))
+(define-test max-subseq-on-partial-length
+             (assert-equal '(4 8 0 7) (max-subseq '(8 4 0 7) 2))
+             (assert-equal '(0 2 3 1 9) (max-subseq '(2 0 3 1 9) 3))
              )
 
-(define-test descending-prefix-full-length
-             (assert-equal '((8 7 4 0)) (descending-prefix '(8 7 4 0)))
+(define-test desc-prefix-full-length
+             (assert-equal '((8 7 4 0)) (desc-prefix '(8 7 4 0)))
              )
 
-(define-test descending-prefix-partial-length
-             (assert-equal '((7 2) 3 4) (descending-prefix '(7 2 3 4)))
-             (assert-equal '((7 5 3) 4) (descending-prefix '(7 5 3 4)))
-             (assert-equal '((1) 5 3 4) (descending-prefix '(1 5 3 4)))
+(define-test desc-prefix-partial-length
+             (assert-equal '((7 2) 3 4) (desc-prefix '(7 2 3 4)))
+             (assert-equal '((7 5 3) 4) (desc-prefix '(7 5 3 4)))
+             (assert-equal '((1) 5 3 4) (desc-prefix '(1 5 3 4)))
              )
 
-(define-test swap-digits-and-reorder
-             (assert-equal '(7 0 4 8) (swap-digits-and-reorder '((4 0) 7 8)))
+(define-test to-swap
+             (assert-equal '((7 0) 8 4) (to-swap '((0 7) 8 4)))
+             (assert-equal '((7 0) 9 7) (to-swap '((0 7) 9 7)))
+             (assert-equal '((5 3 1) 8 4) (to-swap '((1 3 5) 8 4)))
+             )
+
+(define-test to-swap-single-pivot
+             (assert-equal '((7) 8 0 4) (to-swap '((7) 8 0 4)))
+             )
+
+(define-test to-swap-full-length-prefix
+             (assert-equal nil (to-swap '((0 4 7 8))))
+             )
+
+(define-test swap
+             (assert-equal '(0 8 7 4) (swap '((7 0) 8 4)))
+             (assert-equal '(8 7 0 4) (swap '((7) 8 0 4)))
+             )
+
+(define-test swap-nil
+             (assert-equal nil (swap nil))
+             )
+
+(define-test number-
+             (assert-equal 4807 (number- '(7 0 8 4)))
+             )
+
+(define-test max-anagram
+             (assert-equal 98740 (max-anagram 48097))
+             )
+
+(define-test next-anagram
+             (assert-equal 4780 (next-anagram 4807))
+             (assert-equal 4708 (next-anagram 4780))
+             (assert-equal 4087 (next-anagram 4708))
+             )
+
+(define-test next-anagram-last-anagram
+             (assert-equal 0 (next-anagram 478))
              )
 
 (run-tests :all)
+; (run-tests '(next-anagram))
 (sb-ext:quit)
